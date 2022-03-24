ed0d2110d9d74ee26eea3230de529e338c661d43
Luke Street <luke.street@encounterpc.com>
2022-03-23 20:39:27 -0400

    Resolve ctors/dtors/extab/extabindex

--- asm/extab.s
+++ asm/extab.s
@@ -1,6 +1,20 @@
 .include "macros.inc"
 
 .section extab_, "wa"  # 0x800035A0 - 0x800035E0
-.global lbl_extab
-lbl_extab:
-	.incbin "baserom.dol", 0x3C8120, 0x28
+.global __destroy_arr_extab
+__destroy_arr_extab:
+	# ROM: 0x3C8120
+	.4byte 0x20080000
+	.4byte 0
+.global __construct_array_extab
+__construct_array_extab:
+	.4byte 0x20080000
+	.4byte 0x00000060
+	.4byte 0x00000010
+	.4byte 0
+	.4byte 0x82000008
+	.4byte 0x80389844
+.global __partial_array_destructor_extab
+__partial_array_destructor_extab:
+	.4byte 0x18080000
+	.4byte 0
--- asm/extabindex.s
+++ asm/extabindex.s
@@ -2,7 +2,25 @@
 
 .section extabindex_, "wa"  # 0x800035E0 - 0x80003640
 lbl_extabindex:
-	.incbin "baserom.dol", 0x3C8160, 0x24
+	# ROM: 0x3C8160
+	.4byte __destroy_arr
+	.4byte 0x00000078
+	.4byte __destroy_arr_extab
+	.4byte __construct_array
+	.4byte 0x000000F8
+	.4byte __construct_array_extab
+	.4byte __partial_array_destructor
+	.4byte 0x000000B8
+	.4byte __partial_array_destructor_extab
+
 .global lbl_80003604
 lbl_80003604:
-	.incbin "baserom.dol", 0x3C8184, 0x20
+	# ROM: 0x3C8184
+	.4byte lbl_extabindex
+	.4byte lbl_80003604
+	.4byte __destroy_arr
+	.4byte 0x00000228
+	.4byte 0
+	.4byte 0
+	.4byte 0
+	.4byte 0
