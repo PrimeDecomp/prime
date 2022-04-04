58412b389a31a1b98e26704c511781c6f57b5b61
Phillip Stephens <antidote.crk@gmail.com>
2022-04-04 03:20:15 -0700

    Add missing files

---
+++ include/Dolphin/__ppc_eabi_init.h
@@ -0,0 +1,7 @@
+#include "types.h"
+
+
+__declspec(section ".init") extern void __init_hardware(void);
+__declspec(section ".init") extern void __flush_cache(register void* address, register unsigned int size);
+
+extern void __init_cpp(void);
