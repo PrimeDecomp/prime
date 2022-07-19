6acf3a12781197d56d532f53bca4f63710a2fa44
Luke Street <luke.street@encounterpc.com>
2022-07-18 20:30:39 -0400

    Clean up unused tools

--- tools/asmdiff.sh
+++ tools/asmdiff.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -e
-
 OBJDUMP="$DEVKITPPC/bin/powerpc-eabi-objdump -D -bbinary -EB -mpowerpc -M gekko"
 if [ ! -z "$1" ]; then
-OPTIONS="--start-address=$(($1)) --stop-address=$(($2))"
+  OPTIONS="--start-address=$(($1)) --stop-address=$(($2))"
 fi
 $OBJDUMP $OPTIONS baserom.dol > baserom.dump
 $OBJDUMP $OPTIONS build/mp1.0/main.dol > main.dump
