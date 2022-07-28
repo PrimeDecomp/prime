7d56323cd45760140d7b6b3ecc73eb3a2a602045
Phillip Stephens <antidote.crk@gmail.com>
2022-07-28 07:24:49 -0700

    Line match reverb_fx.c and delay_fx.c for S&Gs
    Former-commit-id: 51a84f6ed2607d20b64573abad33857253bf2320

--- src/musyx/reverb_fx.c
+++ src/musyx/reverb_fx.c
@@ -1,5 +1,22 @@
+/* ---------------------------------------
+
+
+
+
+
+
+
+
+   ---------------------------------------
+*/
+
 #include "musyx/musyx_priv.h"
   
+
+  
+  
+
+
 extern bool8 ReverbHICreate(_SND_REVHI_WORK* rev, f32 coloration, f32 time, f32 mix, f32 damping, f32 preDelay, f32 crosstalk);
 extern void ReverbHIFree(_SND_REVHI_WORK* rev);
 
@@ -7,11 +24,15 @@ void sndAuxCallbackReverbHI(u8 reason, SND_AUX_INFO* info, void* user) {
     switch (reason) {
     case SND_AUX_REASON_BUFFERUPDATE:
         if (((SND_AUX_REVERBHI*)user)->tempDisableFX == 0) {
-            ReverbHICallback(info->data.bufferUpdate.left, info->data.bufferUpdate.right, info->data.bufferUpdate.surround, user);
+            ReverbHICallback(info->data.bufferUpdate.left, 
+                             info->data.bufferUpdate.right, 
+                             info->data.bufferUpdate.surround, user);
         }
-        /* fallthrough */
     case SND_AUX_REASON_PARAMETERUPDATE:
-        return;
+        break;
+    default:
+        //ASSERT_MSG(FALSE);
+        break;
     }
 }
 
