6bf68f980874f349487917fb1973f9a3e7a0e947
Phillip Stephens <antidote.crk@gmail.com>
2022-08-11 11:50:15 -0700

    use clang-format on CColor.cpp

--- src/Kyoto/Graphics/CColor.cpp
+++ src/Kyoto/Graphics/CColor.cpp
@@ -1,18 +1,18 @@
 #include "Kyoto/Graphics/CColor.hpp"
 
-const CColor CColor::sBlackColor (0x000000FF);
-const CColor CColor::sWhiteColor (0xFFFFFFFF);
-const CColor CColor::sGreyColor  (0x808080FF);
-const CColor CColor::sRedColor   (0xFF0000FF);
-const CColor CColor::sGreenColor (0x00FF00FF);
-const CColor CColor::sBlueColor  (0x0000FFFF);
+const CColor CColor::sBlackColor(0x000000FF);
+const CColor CColor::sWhiteColor(0xFFFFFFFF);
+const CColor CColor::sGreyColor(0x808080FF);
+const CColor CColor::sRedColor(0xFF0000FF);
+const CColor CColor::sGreenColor(0x00FF00FF);
+const CColor CColor::sBlueColor(0x0000FFFF);
 const CColor CColor::sYellowColor(0xFFFF00FF);
 const CColor CColor::sPurpleColor(0xA000FFFF);
 const CColor CColor::sOrangeColor(0xFF7000FF);
 
 const CColor& CColor::Black() { return sBlackColor; }
 const CColor& CColor::White() { return sWhiteColor; }
-const CColor& CColor::Grey()  { return sGreyColor; }
+const CColor& CColor::Grey() { return sGreyColor; }
 const CColor& CColor::Red() { return sRedColor; }
 const CColor& CColor::Green() { return sGreenColor; }
 const CColor& CColor::Blue() { return sBlueColor; }
