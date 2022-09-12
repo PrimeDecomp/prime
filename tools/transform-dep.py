c6823f56cd0cc51ee2274e46a3de92a73ba3c1f0
Luke Street <luke.street@encounterpc.com>
2022-09-11 21:07:45 -0400

    Update Makefile & transform-dep.py
    Former-commit-id: 38acae22fea0ec822edb16fd3a04fc796167c2d4

--- tools/transform-dep.py
+++ tools/transform-dep.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 import argparse
 import os
+from platform import uname
 
 wineprefix = os.path.join(os.environ['HOME'], '.wine')
 if 'WINEPREFIX' in os.environ:
   wineprefix = os.environ['WINEPREFIX']
 winedevices = os.path.join(wineprefix, 'dosdevices')
+
+def in_wsl() -> bool:
+    return 'microsoft-standard' in uname().release
 
 def import_d_file(in_file) -> str:
     out_text = ''
@@ -26,8 +30,15 @@ def import_d_file(in_file) -> str:
             path = line.strip()
           # lowercase drive letter
           path = path[0].lower() + path[1:]
-          # use $WINEPREFIX/dosdevices to resolve path
-          path = os.path.realpath(os.path.join(winedevices, path.replace('\\', '/')))
+          if path[0] == 'z':
+            # shortcut for z:
+            path = path[2:].replace('\\', '/')
+          elif in_wsl():
+            path = path[0:1] + path[2:]
+            path = os.path.join('/mnt', path.replace('\\', '/'))
+          else:
+            # use $WINEPREFIX/dosdevices to resolve path
+            path = os.path.realpath(os.path.join(winedevices, path.replace('\\', '/')))
           out_text += "\t" + path + suffix + "\n"
 
     return out_text
