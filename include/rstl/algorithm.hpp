1e59dd91fcddd3c4727d8e9aa5238438917269f9
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-09-30 14:49:03 +0300

    algorithm.hpp header fixes
    Former-commit-id: 675a1904c4496aa699578969eef6d7429bfb43c8

--- include/rstl/algorithm.hpp
+++ include/rstl/algorithm.hpp
@@ -1,18 +1,10 @@
-#ifndef _RSTL_MATH_HPP
-#define _RSTL_MATH_HPP
+#ifndef _RSTL_ALGORITHM_HPP
+#define _RSTL_ALGORITHM_HPP
 
 #include "rstl/pointer_iterator.hpp"
 
 namespace rstl {
 template < typename T >
-inline const T& min_val(const T& a, const T& b) {
-  return (b < a) ? b : a;
-}
-
-template < typename T >
-inline const T& max_val(const T& a, const T& b) {
-  return (a < b) ? b : a;
-}
 
 template < class Iter, class T >
 inline Iter find(Iter first, Iter last, const T& val) {
@@ -21,4 +13,4 @@ inline Iter find(Iter first, Iter last, const T& val) {
   return first;
 }
 } // namespace rstl
-#endif // _RSTL_MATH_HPP
+#endif // _RSTL_ALGORITHM_HPP
