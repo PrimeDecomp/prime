df58a7e6901836b374f3437549b8d92061c287d3
Phillip Stephens <antidote.crk@gmail.com>
2022-10-06 02:27:07 -0700

    Add missing include
    Former-commit-id: a37c4fde2e21ebdcdca3952eda66c688f67563f7

---
+++ include/Kyoto/SObjectTag.hpp
@@ -0,0 +1,20 @@
+#ifndef __SOBJECTTAG_HPP__
+#define __SOBJECTTAG_HPP__
+
+#define kInvalidAssetId 0xFFFFFFFFu
+
+typedef uint CAssetId;
+typedef uint FourCC;
+
+struct SObjectTag {
+  FourCC type;
+  CAssetId id;
+
+  SObjectTag() {}
+  SObjectTag(FourCC type, CAssetId id) : type(type), id(id) {}
+  SObjectTag(const SObjectTag& other) : type(other.type), id(other.id) {}
+
+  static const char* Type2Text(FourCC type);
+};
+
+#endif
