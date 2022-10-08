50453f5b7cb42311c1717a3cd18a42b16f7ddd1e
Phillip Stephens <antidote.crk@gmail.com>
2022-10-07 23:55:54 -0700

    Match and link CScriptSwitch
    Former-commit-id: 9cfd7f1776c837f8a2a7ea9221051b2b2774167c

---
+++ src/MetroidPrime/ScriptObjects/CScriptSwitch.cpp
@@ -0,0 +1,36 @@
+#include "MetroidPrime/ScriptObjects/CScriptSwitch.hpp"
+
+CScriptSwitch::CScriptSwitch(TUniqueId uid, const rstl::string& name, const CEntityInfo& info,
+                             bool active, bool opened, bool closeOnOpened)
+: CEntity(uid, info, active, name) {
+  mOpened = opened;
+  mCloseOnOpened = closeOnOpened;
+}
+
+void CScriptSwitch::Accept(IVisitor& visitor) { visitor.Visit(*this); }
+
+void CScriptSwitch::AcceptScriptMsg(EScriptObjectMessage msg, TUniqueId objId, CStateManager& mgr) {
+  if (GetActive()) {
+    switch (msg) {
+    case kSM_Open:
+      mOpened = true;
+      break;
+    case kSM_Close:
+      mOpened = false;
+      break;
+    case kSM_SetToZero: {
+      if (mOpened) {
+        SendScriptMsgs(kSS_Open, mgr, kSM_None);
+        if (mCloseOnOpened)
+          mOpened = false;
+      } else {
+        SendScriptMsgs(kSS_Closed, mgr, kSM_None);
+      }
+      break;
+    }
+    }
+  }
+  CEntity::AcceptScriptMsg(msg, objId, mgr);
+}
+
+CScriptSwitch::~CScriptSwitch() {}
