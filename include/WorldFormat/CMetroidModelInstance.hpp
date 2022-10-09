cf99f3047b95fa6e9a6da31b640257f52a20c994
Phillip Stephens <antidote.crk@gmail.com>
2022-10-09 15:43:43 -0700

    Near match CMetroidModelInstances, regswaps in BoundingBoxFromData

--- include/WorldFormat/CMetroidModelInstance.hpp
+++ include/WorldFormat/CMetroidModelInstance.hpp
@@ -1,18 +1,28 @@
 #ifndef _CMETROIDMODELINSTANCE
 #define _CMETROIDMODELINSTANCE
 
-#include "Kyoto/Math/CTransform4f.hpp"
 #include "Kyoto/Math/CAABox.hpp"
+#include "Kyoto/Math/CTransform4f.hpp"
 
 #include "rstl/vector.hpp"
 
 class CMetroidModelInstance {
 public:
-  CMetroidModelInstance(const void* header, const void* firstGeom,
-                                               const void* positions, const void* normals,
-                                               const void* colors, const void* uvs,
-                                               const void* packedUvs,
-                                               const rstl::vector< void* >& surfaces);
+  CMetroidModelInstance(const void* header, const void* firstGeom, const void* positions,
+                        const void* normals, const void* colors, const void* texCoords,
+                        const void* packedTexCoords, const rstl::vector< void* >& surfaces);
+
+private:
+  u32 x0_visorFlags;
+  CTransform4f x4_worldXf;
+  CAABox x34_worldAABB;
+  const void* x4c_materialData;
+  rstl::vector<void*> x50_surfaces;
+  const void* x60_positions;
+  const void* x64_normals;
+  const void* x68_colors;
+  const void* x6c_texCoords;
+  const void* x70_packedTexCoords;
 };
 
 #endif // _CMETROIDMODELINSTANCE
