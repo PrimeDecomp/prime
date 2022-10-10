91943cb93ec399498536e82fb1761515d00e16b3
Phillip Stephens <antidote.crk@gmail.com>
2022-10-10 01:46:54 -0700

    Match and link more of Runtime

---
+++ src/Runtime/MWCPlusLib.h
@@ -0,0 +1,15 @@
+#ifndef _MWCPLUSLIB
+#define _MWCPLUSLIB
+
+#include <types.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void* __copy(char* to, char* from, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _MWCPLUSLIB
