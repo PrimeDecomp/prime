b6b39b741200a32275616f2c28646fb074128afe
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-10-16 18:59:40 +0300

    Add CTweakPlayerControl
    Former-commit-id: eec8e2904451c442d6471de92261f37fe16c6531

---
+++ src/MetroidPrime/Tweaks/CTweakPlayerControl.cpp
@@ -0,0 +1,23 @@
+#include "MetroidPrime/Tweaks/CTweakPlayerControl.hpp"
+
+#include "Kyoto/Streams/CInputStream.hpp"
+
+CTweakPlayerControl::~CTweakPlayerControl() {}
+
+rstl::reserved_vector< ControlMapper::EFunctionList, 67 > LoadMappings(CInputStream& in) {
+  rstl::reserved_vector< ControlMapper::EFunctionList, 67 > result;
+  for (int i = 0; i < result.capacity(); ++i) {
+    result.push_back(static_cast<ControlMapper::EFunctionList>(in.ReadLong()));
+  }
+  return result;
+}
+
+CTweakPlayerControl::CTweakPlayerControl(CInputStream& in) : m_mappings(LoadMappings(in)) {}
+
+ControlMapper::EFunctionList
+CTweakPlayerControl::GetMapping(ControlMapper::ECommands command) const {
+  if (command < ControlMapper::kC_Forward || command > ControlMapper::kC_UNKNOWN - 1)
+    return m_mappings[0];
+  
+  return m_mappings[command];
+}
