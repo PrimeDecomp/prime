d00cfae24ee63098fe2167800b31380d60c5a3a3
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-10-18 02:53:49 +0300

    Improving match of CPhysicsActor
    Former-commit-id: 629a01420729a00a7e29183167fddcd2bf654cca

--- include/Kyoto/Math/CNUQuaternion.hpp
+++ include/Kyoto/Math/CNUQuaternion.hpp
@@ -4,12 +4,18 @@
 #include "types.h"
 
 class CMatrix3f;
+class CVector3f;
+class CQuaternion;
+
 class CNUQuaternion {
 public:
   CNUQuaternion(float w, float x, float y, float z) : w(w), x(x), y(y), z(z) {}
+  CNUQuaternion(float, const CVector3f&);
 
   static CNUQuaternion BuildFromMatrix3f(const CMatrix3f& matrix);
   static CNUQuaternion BuildFromQuaternion(const CQuaternion& quat);
+
+  CNUQuaternion operator*(const CNUQuaternion&) const;
 
 private:
   float w;
@@ -17,5 +23,8 @@ private:
   float y;
   float z;
 };
+
+CNUQuaternion operator*(float f, const CNUQuaternion&);
+CNUQuaternion operator*(const CNUQuaternion&, float f);
 
 #endif // _CNUQUATERNION
