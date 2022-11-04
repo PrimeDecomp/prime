fe02b8054560746dd18ab359739a1ec92356cbda
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-11-04 12:50:34 +0200

    Mostly add CPhysicsActor::GetRotateToORAngularMomentumWR

--- include/Kyoto/Math/CQuaternion.hpp
+++ include/Kyoto/Math/CQuaternion.hpp
@@ -71,6 +71,9 @@ public:
   float GetX() const { return x; }
   float GetY() const { return y; }
   float GetZ() const { return z; }
+  const CVector3f& GetImaginary() const { 
+    // TODO: hack! has a Vector3f field?
+    return *reinterpret_cast<const CVector3f*>(&x); }
 
 private:
   float w;
