0e45d770364322de1c3f1ed0fe36db5536d5534b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-11-17 07:13:56 +0200

    Use raw pointers in CWeaponDescription, link it

--- include/Weapons/CWeaponDescription.hpp
+++ include/Weapons/CWeaponDescription.hpp
@@ -22,16 +22,16 @@ public:
 
   CWeaponDescription();
   ~CWeaponDescription();
-  rstl::single_ptr< CVectorElement > x0_IORN;
-  rstl::single_ptr< CVectorElement > x4_IVEC;
-  rstl::single_ptr< CVectorElement > x8_PSOV;
-  rstl::single_ptr< CModVectorElement > xc_PSVM;
+  CVectorElement* x0_IORN;
+  CVectorElement* x4_IVEC;
+  CVectorElement* x8_PSOV;
+  CModVectorElement* xc_PSVM;
   bool x10_VMD2;
-  rstl::single_ptr< CIntElement > x14_PSLT;
-  rstl::single_ptr< CVectorElement > x18_PSCL;
-  rstl::single_ptr< CColorElement > x1c_PCOL;
-  rstl::single_ptr< CVectorElement > x20_POFS;
-  rstl::single_ptr< CVectorElement > x24_OFST;
+  CIntElement* x14_PSLT;
+  CVectorElement* x18_PSCL;
+  CColorElement* x1c_PCOL;
+  CVectorElement* x20_POFS;
+  CVectorElement* x24_OFST;
   bool x28_APSO;
   bool x29_HOMG;
   bool x2a_AP11;
@@ -39,7 +39,7 @@ public:
   bool x2c_AS11;
   bool x2d_AS12;
   bool x2e_AS13;
-  rstl::single_ptr< CRealElement > x30_TRAT;
+  CRealElement* x30_TRAT;
   TChildGeneratorDesc x34_APSM;
   TChildGeneratorDesc x44_APS2;
   TSwooshGeneratorDesc x54_ASW1;
@@ -51,8 +51,8 @@ public:
   bool xa5_LWTR;
   bool xa6_SWTR;
   int xa8_PJFX;
-  rstl::single_ptr< CRealElement > xac_RNGE;
-  rstl::single_ptr< CRealElement > xb0_FOFF;
+  CRealElement* xac_RNGE;
+  CRealElement* xb0_FOFF;
 };
 
 #endif // _CWEAPONDESCRIPTION
