41f47c198dd745a3508bf8860f1d57e79889ae29
Phillip Stephens <antidote.crk@gmail.com>
2022-11-29 23:07:31 -0800

    Add musyx/stream.c, match and link arith.c
    Former-commit-id: 7bd0748d30892d576227fc31a3ca42857fac50d1

--- asm/Runtime/ansi_fp.s
+++ asm/Runtime/ansi_fp.s
@@ -2248,12 +2248,12 @@ lbl_8038CE14:
 /* 8038CE14 00389D74  7C 83 23 78 */	mr r3, r4
 /* 8038CE18 00389D78  4E 80 00 20 */	blr
 
-.global abs
-abs:
-/* 8038CE1C 00389D7C  7C 64 FE 70 */	srawi r4, r3, 0x1f
-/* 8038CE20 00389D80  7C 80 1A 78 */	xor r0, r4, r3
-/* 8038CE24 00389D84  7C 64 00 50 */	subf r3, r4, r0
-/* 8038CE28 00389D88  4E 80 00 20 */	blr
+#.global abs
+#abs:
+#/* 8038CE1C 00389D7C  7C 64 FE 70 */	srawi r4, r3, 0x1f
+#/* 8038CE20 00389D80  7C 80 1A 78 */	xor r0, r4, r3
+#/* 8038CE24 00389D84  7C 64 00 50 */	subf r3, r4, r0
+#/* 8038CE28 00389D88  4E 80 00 20 */	blr
 
 .section .sdata2, "a"
 .balign 8
---
+++ src/Runtime/arith.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+int abs(int n) {
+  if (n < 0)
+    return (-n);
+  else
+    return (n);
+}
+
+long labs(long n) {
+  if (n < 0)
+    return (-n);
+  else
+    return (n);
+}
