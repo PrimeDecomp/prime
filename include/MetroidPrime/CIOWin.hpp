58ea53f21de4f444935f27e4820a21ecb58bcfe3
Luke Street <luke.street@encounterpc.com>
2022-12-07 00:52:23 -0500

    CMainFlow improvements

--- include/MetroidPrime/CIOWin.hpp
+++ include/MetroidPrime/CIOWin.hpp
@@ -30,4 +30,16 @@ private:
   rstl::string x4_name;
 };
 
+#define kFrontEndUIMsgPriority 12
+#define kFrontEndUIDrawPriority 11
+#define kMFGameMsgPriority 10
+#define kMFGameDrawPriority 1000
+
+// namespace IOWinPriorities {
+// static int kFrontEndUIMsgPriority = 12;
+// static int kFrontEndUIDrawPriority = 11;
+// static int kMFGameMsgPriority = 10;
+// static int kMFGameDrawPriority = 1000;
+// } // namespace IOWinPriorities
+
 #endif // _CIOWIN
