abef49951ce7bb80261f1e263db7ce95482fc931
Luke Street <luke.street@encounterpc.com>
2022-12-07 00:13:59 -0500

    Minor CEulerAngles progress
    Former-commit-id: 67f01bc9cd41528478f6f153dc3f9dd1c07567d1

--- include/Kyoto/Math/CMatrix3f.hpp
+++ include/Kyoto/Math/CMatrix3f.hpp
@@ -3,6 +3,7 @@
 
 #include "types.h"
 
+#include "Kyoto/Math/CTransform4f.hpp"
 #include "Kyoto/Math/CVector3f.hpp"
 
 class CMatrix3f {
@@ -10,6 +11,13 @@ class CMatrix3f {
 
 public:
   CMatrix3f(const CMatrix3f&);
+  CMatrix3f(const CVector3f& m0, const CVector3f& m1, const CVector3f& m2);
+  // fake but useful for CEulerAngles?
+  CMatrix3f(const CTransform4f& xf);/*
+  : m0(xf.GetRow(kDX))
+  , m1(xf.GetRow(kDY))
+  , m2(xf.GetRow(kDZ)) {}*/
+
   const CMatrix3f& operator=(const CMatrix3f& other);
 
   static const CMatrix3f& Identity() { return sIdentity; }
@@ -21,7 +29,21 @@ public:
   inline const CVector3f& GetRow(EDimY dim) const { return m1; }
   inline const CVector3f& GetRow(EDimZ dim) const { return m2; }
 
+  float Get00() const { return m0.GetX(); }
+  float Get01() const { return m0.GetY(); }
+  float Get02() const { return m0.GetZ(); }
+  float Get10() const { return m1.GetX(); }
+  float Get11() const { return m1.GetY(); }
+  float Get12() const { return m1.GetZ(); }
+  float Get20() const { return m2.GetX(); }
+  float Get21() const { return m2.GetY(); }
+  float Get22() const { return m2.GetZ(); }
+
   inline CVector3f GetColumn(EDimY dim) const { return CVector3f(m0.GetY(), m1.GetY(), m2.GetY()); }
+
+  static CMatrix3f FromTransform(const CTransform4f& xf);/* {
+    return CMatrix3f(xf.GetRow(kDX), xf.GetRow(kDY), xf.GetRow(kDZ));
+  }*/
 
 private:
   // TODO maybe individual floats
