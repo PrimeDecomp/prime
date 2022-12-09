4a964152376c2436c40ea6c00175f55b7a47e80f
Phillip Stephens <antidote.crk@gmail.com>
2022-12-09 13:00:39 -0800

    Add CCharAnimTime
    Former-commit-id: f7e3f059b2bfad84bfb8a7349dc255c9c7108f6d

--- include/Kyoto/Animation/CCharAnimTime.hpp
+++ include/Kyoto/Animation/CCharAnimTime.hpp
@@ -3,6 +3,7 @@
 
 #include "types.h"
 
+class COutputStream;
 class CInputStream;
 class CCharAnimTime {
 public:
@@ -16,13 +17,25 @@ public:
   float GetSeconds() const { return x0_time; }
 
   explicit CCharAnimTime(CInputStream& in);
+  explicit CCharAnimTime(float time);
   explicit CCharAnimTime(EType type, float time) : x0_time(time), x4_type(type) {}
   CCharAnimTime(const CCharAnimTime& other) : x0_time(other.x0_time), x4_type(other.x4_type) {}
 
-  bool operator!=(const CCharAnimTime& other) const;
   bool operator>(const CCharAnimTime& other) const;
+  bool operator==(const CCharAnimTime& other) const;
+  bool operator!=(const CCharAnimTime& other) const;
   bool operator<(const CCharAnimTime& other) const;
-  
+  CCharAnimTime operator/(const CCharAnimTime& other) const;
+  CCharAnimTime operator*(const float& other) const;
+  CCharAnimTime operator-(const CCharAnimTime& other) const;
+  CCharAnimTime operator+(const CCharAnimTime& other) const;
+  const CCharAnimTime& operator+=(const CCharAnimTime& other);
+  const CCharAnimTime& operator-=(const CCharAnimTime& other);
+  bool operator<=(const CCharAnimTime& other) const;
+  bool operator>=(const CCharAnimTime& other) const;
+  bool GreaterThanZero() const;
+  bool EqualsZero() const;
+  void PutTo(COutputStream& out) const;
   static CCharAnimTime Infinity() { return CCharAnimTime(kT_Infinity, 1.0f); }
 
 private:
