e5e30d8e9517bd2ba8bf598872ce74588d52990c
Ryan Burns <52847440+r-burns@users.noreply.github.com>
2022-12-17 17:45:27 -0800

    Match OSArena (#15)
    Former-commit-id: 25c687bd02e3c88212cff46bf6092a0901d6b2fe

--- include/dolphin/os/OSArena.h
+++ include/dolphin/os/OSArena.h
@@ -1,4 +1,13 @@
 #ifndef _DOLPHIN_OSARENA
 #define _DOLPHIN_OSARENA
 
+#include <dolphin/types.h>
+
+void* OSGetArenaHi(void);
+void* OSGetArenaLo(void);
+void OSSetArenaHi(void* addr);
+void OSSetArenaLo(void* addr);
+void* OSAllocFromArenaLo(u32 size, u32 align);
+void* OSAllocFromArenaLo(u32 size, u32 align);
+
 #endif // _DOLPHIN_OSARENA
