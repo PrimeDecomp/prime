61bd16d0c79c4a426ca0f1c3c5c6cc770a26c22a
Phillip Stephens <antidote.crk@gmail.com>
2023-01-11 18:47:23 -0800

    Fix Splelling fial
    Former-commit-id: 94922577d26601b3b933ba2db4ec9499e1bf867d

--- include/Kyoto/Animation/CPASParmInfo.hpp
+++ include/Kyoto/Animation/CPASParmInfo.hpp
@@ -16,12 +16,12 @@ public:
 
   CPASParmInfo(CInputStream& in);
 
-  CPASAnimInfo::EParmType GetParamterType() const { return x0_type; }
+  CPASAnimInfo::EParmType GetParameterType() const { return x0_type; }
   EWeightFunction GetWeightFunction() const { return x4_weightFunction; }
   float GetWeight() const { return x8_weight; }
   CPASAnimParm::UParmValue GetParameterMinValue() const { return xc_min; }
   CPASAnimParm::UParmValue GetParameterMaxValue() const { return x10_max; }
-  
+
 private:
   CPASAnimParm::EParmType x0_type;
   EWeightFunction x4_weightFunction;
