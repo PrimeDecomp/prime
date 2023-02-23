411a21699f9b781a34cbd0d03482798a22bedf45
Phillip Stephens <antidote.crk@gmail.com>
2023-02-22 23:04:07 -0800

    Reorganize MusyX source
    Former-commit-id: cbe676015f0a82430b7f9d66413d0a36495e636b

--- include/musyx/assert.h
+++ include/musyx/assert.h
@@ -5,12 +5,12 @@ extern void OSPanic(const char* file, int line, const char* msg, ...);
 
 #ifndef ASSERT
 #ifdef _DEBUG
-#define ASSERT(cond) \
-  do { \
-    if (!cond) { \
-      OSPanic(__FILE__, __LINE__, "Failed assertion " #cond); \
-    } \
-  } while(0)
+#define ASSERT(cond)                                                                               \
+  do {                                                                                             \
+    if (!cond) {                                                                                   \
+      OSPanic(__FILE__, __LINE__, "Failed assertion " #cond);                                      \
+    }                                                                                              \
+  } while (0)
 #else
 #define ASSERT(cond)
 #endif
@@ -18,14 +18,22 @@ extern void OSPanic(const char* file, int line, const char* msg, ...);
 
 #ifndef ASSERT_MSG
 #ifdef _DEBUG
-#define ASSERT_MSG(cond, msg) \
-  do { \
-    if (!cond) { \
-      OSPanic(__FILE__, __LINE__, msg); \
-    } \
-  } while(0)
+#define ASSERT_MSG(cond, msg)                                                                      \
+  do {                                                                                             \
+    if (!cond) {                                                                                   \
+      OSPanic(__FILE__, __LINE__, msg);                                                            \
+    }                                                                                              \
+  } while (0)
 #else
 #define ASSERT_MSG(cond, msg)
+#endif
+#endif
+
+#ifndef MUSY_DEBUG
+#if _DEBUG
+#define MUSY_DEBUG OSReport
+#else
+#define MUSY_DEBUG
 #endif
 #endif
 
