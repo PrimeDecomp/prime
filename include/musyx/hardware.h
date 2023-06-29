f60291726ab2d62338c659c36a8950a39cbbb25e
Luke Street <luke.street@encounterpc.com>
2023-06-29 19:12:29 -0400

    Major seq.c progress & musyx header cleanup

--- include/musyx/hardware.h
+++ include/musyx/hardware.h
@@ -1,13 +1,13 @@
 #ifndef _MUSYX_HARDWARE
 #define _MUSYX_HARDWARE
 
-
 #include "musyx/musyx_priv.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-u32 hwIsStudioActive(u8 studio);
+bool hwIsStudioActive(u8 studio);
 
 #ifdef __cplusplus
 }
