c559a5c3e729164ecd46953196a10da5340560a5
Phillip Stephens <antidote.crk@gmail.com>
2023-09-26 13:49:16 -0700

    Fix local functions in snd3d.s, add missing functions in snd_init.c, nearly match dataInsertKeymap
    Former-commit-id: 919e53661f7ddf571db036f5c5e1ed364a09003e

--- src/musyx/runtime/snd_init.c
+++ src/musyx/runtime/snd_init.c
@@ -1,25 +1,77 @@
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
 #include "musyx/musyx_priv.h"
-//#define _DEBUG
+// #define _DEBUG
+
+/*
 
 
-s32 DoInit(u32 rate, u32 aramSize, u8 voices, u32 flags) {
+
+
+*/
+static s32 DoInit(u32 rate, u32 aramSize, u32 voices, u32 flags) {
+  bool ret;
+
+  MUSY_DEBUG("\nMusyX software initialization...\nBuild Date: %s %s\n\n", __DATE__, __TIME__);
+  ret = FALSE;
+
   dataInitStack();
+
   dataInit(0, aramSize);
+
   seqInit();
+
   synthIdleWaitActive = 0;
+
   synthInit(rate, voices);
+
   streamInit();
+
   vsInit();
+
   s3dInit(flags);
+
   sndActive = 1;
 
-  return FALSE;
+  MUSY_DEBUG("MusyX logic is initialized.\n\n");
+
+  return ret;
 }
 
-s32 sndInit(u8 voices, u8 music, u8 sfx, u8 studios, u32 flags, u32 aramSize) {
-  u32 rate;
-  s32 ret;
+/*
 
+
+
+
+
+
+
+
+
+
+*/
+s32 sndInit(u8 voices, u8 music, u8 sfx, u8 studios, u32 flags, u32 aramSize) {
+  s32 ret; // r31
+  u32 frq; // r1+0x14
+
+  MUSY_DEBUG("Entering sndInit()\n\n");
   sndActive = 0;
   if (voices <= 64) {
     synthInfo.voiceNum = voices;
@@ -31,27 +83,65 @@ s32 sndInit(u8 voices, u8 music, u8 sfx, u8 studios, u32 flags, u32 aramSize) {
   } else {
     synthInfo.studioNum = 8;
   }
-  rate = 32000;
+
   synthInfo.maxMusic = music;
   synthInfo.maxSFX = sfx;
-  ret = hwInit(&rate, synthInfo.voiceNum, synthInfo.studioNum, flags);
-  if (ret == 0) {
+  frq = 32000;
+  if ((ret = hwInit(&frq, synthInfo.voiceNum, synthInfo.studioNum, flags)) == 0) {
     ret = DoInit(32000, aramSize, synthInfo.voiceNum, flags);
   }
+
+  MUSY_DEBUG("Leaving sndInit().\n\n");
   return ret;
 }
 
+/* */
 void sndQuit() {
+  MUSY_ASSERT_MSG(sndActive, "Sound system is not initialized.");
+
   hwExit();
+  /*
+
+
+  */
   dataExit();
+  /*
+   */
   s3dExit();
+  /* */
   synthExit();
+  /* */
   sndActive = 0;
 }
 
+/*
+
+
+*/
 void sndSetMaxVoices(u8 music, u8 sfx) {
-  MUSY_ASSERT_MSG(music > synthInfo.voiceNum, "Music voices are above maximum voice number.");
-  MUSY_ASSERT_MSG(sfx > synthInfo.voiceNum, "Sfx voices are above maximum voice number.");
+  MUSY_ASSERT_MSG(music <= synthInfo.voiceNum, "Music voices are above maximum voice number.");
+  MUSY_ASSERT_MSG(sfx <= synthInfo.voiceNum, "Sfx voices are above maximum voice number.");
+
   synthInfo.maxMusic = music;
   synthInfo.maxSFX = sfx;
+}
+
+/*
+
+
+*/
+bool sndIsInstalled() { return sndActive; }
+
+/*
+
+
+
+
+*/
+SND_PLAYBACKINFO* sndGetPlayBackInfo() {
+  if (sndActive) {
+    return &synthInfo.pbInfo;
+  }
+
+  return NULL;
 }
