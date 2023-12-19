5442a6f9b76b5bbf7aed93f896b663719b2a97c3
Phillip Stephens <antidote.crk@gmail.com>
2023-12-18 18:42:55 -0800

    Convert MusyX to a submodule
    Former-commit-id: 465620fe5729ceb87953fc5d6a66f9453871348a

--- src/musyx/runtime/hw_lib_dummy.c
+++
@@ -1,4 +0,0 @@
-#include "musyx/musyx.h"
-
-u32 aramSize = 0;
-u8* aramBase = NULL;
--- src/musyx/runtime/hw_memory.c
+++
@@ -1,5 +0,0 @@
-#include "musyx/hardware.h"
-
-void* salMalloc(u32 len) { return salHooks.malloc(len); }
-
-void salFree(void* addr) { salHooks.free(addr); }
