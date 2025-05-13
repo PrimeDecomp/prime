590eadf51a1910a31f4fbddb4ef1838dd12338a0
Phillip Stephens <antidote.crk@gmail.com>
2025-05-13 12:12:51 -0700

    Fix build

--- src/Kyoto/CFactoryStore.cpp
+++ src/Kyoto/CFactoryStore.cpp
@@ -3,16 +3,16 @@
 
 struct CDummyFactory : public IObjectStore {
 public:
-  CDummyFactory(IFactory& factory) : mFactory(factory){};
-  CToken GetObj(const SObjectTag& tag, CVParamTransfer xfer) { return CToken(); }
-  CToken GetObj(const SObjectTag& tag) { return CToken(); };
-  CToken GetObj(const char* name) { return CToken(); };
-  CToken GetObj(const char* name, CVParamTransfer xfer) { return CToken(); };
-  bool HasObject(const SObjectTag& tag) { return false; }
-  bool ObjectIsLive(const SObjectTag& tag) { return false; }
-  IFactory& GetFactory() { return mFactory; }
-  void Flush(){};
-  void ObjectUnreferenced(const SObjectTag& tag){};
+  CDummyFactory(IFactory& factory) : mFactory(factory) {};
+  CToken GetObj(const SObjectTag& tag, const CVParamTransfer& xfer) override { return CToken(); }
+  CToken GetObj(const SObjectTag& tag) override{ return CToken(); };
+  CToken GetObj(const char* name) override{ return CToken(); };
+  CToken GetObj(const char* name, const CVParamTransfer& xfer) override { return CToken(); };
+  bool HasObject(const SObjectTag& tag) const override{ return false; }
+  bool ObjectIsLive(const SObjectTag& tag) const override{ return false; }
+  IFactory& GetFactory() const override{ return mFactory; }
+  void Flush() override{};
+  void ObjectUnreferenced(const SObjectTag& tag) override{};
 
 private:
   IFactory& mFactory;
