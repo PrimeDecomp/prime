eb9a350739dd904413ca68f8d5624e16e8adba4e
Phillip Stephens <antidote.crk@gmail.com>
2025-08-17 22:37:00 -0700

    Initial CSpawnSystemKeyframeData

--- include/Kyoto/Particles/CSpawnSystemKeyframeData.hpp
+++ include/Kyoto/Particles/CSpawnSystemKeyframeData.hpp
@@ -1,12 +1,25 @@
 #ifndef _CSPAWNSYSTEMKEYFRAMEDATA
 #define _CSPAWNSYSTEMKEYFRAMEDATA
 
+#include <rstl/pair.hpp>
+#include <rstl/vector.hpp>
+
+class CInputStream;
 class CSpawnSystemKeyframeData {
 public:
+  class CSpawnSystemKeyframeInfo {
+  public:
+    CSpawnSystemKeyframeInfo(CInputStream& in);
+  };
+  CSpawnSystemKeyframeData(CInputStream& in);
   ~CSpawnSystemKeyframeData();
 
 private:
-  char data[0x20];
+  int mUnknown1;
+  int mUnknown2;
+  int mEndFrame;
+  int mUnknown3;
+  rstl::vector< rstl::pair< uint, rstl::vector< CSpawnSystemKeyframeInfo > > > mFrames;
 };
 
-#endif
+#endif // _CSPAWNSYSTEMKEYFRAMEDATA
