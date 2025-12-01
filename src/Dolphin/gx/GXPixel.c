923a28b63500fe6033944943edbf7aacae2fb82c
samuelroyal <66704279+samuelroyal@users.noreply.github.com>
2025-12-01 12:55:46 -0500

    95% match GXSetColorUpdate & GXSetAlphaUpdate (regswap)

---
+++ src/Dolphin/gx/GXPixel.c
@@ -0,0 +1,33 @@
+#include "dolphin/gx.h"
+#include "dolphin/gx/GXPriv.h"
+
+void GXSetFog(GXFogType type, f32 startz, f32 endz, f32 nearz, f32 farz, GXColor color);
+void GXSetFogColor(GXColor color);
+// ? GXSetFogRangeAdj();
+void GXSetBlendMode(GXBlendMode type, GXBlendFactor src_factor, GXBlendFactor dst_factor,
+                    GXLogicOp op);
+void GXSetColorUpdate(GXBool update_enable) {
+  GXData* gxdata = gx;
+  u32 r6 = GX_BITFIELD(gxdata->cmode0, 28, 1, update_enable);
+
+  GX_WRITE_RA_REG(r6);
+
+  gxdata->cmode0 = r6;
+  gxdata->bpSentNot = GX_FALSE;
+};
+void GXSetAlphaUpdate(GXBool update_enable) {
+  GXData* gxdata = gx;
+  u32 r6 = GX_BITFIELD(gxdata->cmode0, 27, 1, update_enable);
+
+  GX_WRITE_RA_REG(r6);
+
+  gxdata->cmode0 = r6;
+  gxdata->bpSentNot = GX_FALSE;
+};
+void GXSetZMode(GXBool compare_enable, GXCompare func, GXBool update_enable);
+void GXSetZCompLoc(GXBool before_tex);
+void GXSetPixelFmt(GXPixelFmt pix_fmt, GXZFmt16 z_fmt);
+void GXSetDither(GXBool dither);
+void GXSetDstAlpha(GXBool enable, u8 alpha);
+// ? GXSetFieldMask();
+void GXSetFieldMode(u8 field_mode, u8 half_aspect_ratio);
