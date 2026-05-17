ca535ad9bff55878a57cf7ebf4b526984b2eb34f
Efraim2704 <elefri27@hotmail.com>
2026-05-17 23:28:10 +0200

    Revert IMetaAnim.hpp to original

--- include/Kyoto/Animation/IMetaAnim.hpp
+++ include/Kyoto/Animation/IMetaAnim.hpp
@@ -1,10 +1,13 @@
 #ifndef _IMETAANIM
 #define _IMETAANIM
+
 #include "Kyoto/Animation/CCharAnimTime.hpp"
+
 #include <rstl/optional_object.hpp>
 #include <rstl/rc_ptr.hpp>
 #include <rstl/set.hpp>
 #include <rstl/string.hpp>
+
 enum EMetaAnimType {
   kMAT_Play,
   kMAT_Blend,
@@ -12,35 +15,44 @@ enum EMetaAnimType {
   kMAT_Random,
   kMAT_Sequence,
 };
+
 class CAnimTreeNode;
 class CPrimitive;
 class CCharAnimTime;
 class IAnimReader;
 class CAnimSysContext;
+
 class CPreAdvanceIndicator {
   bool mIsTime;
   CCharAnimTime mTime;
   const char* mString;
   uint x10_[11];
   ushort x3c_;
+
 public:
   explicit CPreAdvanceIndicator(const CCharAnimTime& time) : mIsTime(true), mTime(time), mString(0) {}
   explicit CPreAdvanceIndicator(const char* string) : mIsTime(false), mString(string) {}
   bool IsTime() const;
   const CCharAnimTime& GetTime() const;
+
   bool IsString() const;
   const char* const& GetString() const;
 };
+
 class CMetaAnimTreeBuildOrders {
 public:
   static CMetaAnimTreeBuildOrders NoSpecialOrders();
   static CMetaAnimTreeBuildOrders PreAdvanceForAll(const CPreAdvanceIndicator& ind);
+
+private:
   rstl::optional_object< CPreAdvanceIndicator > mRecursiveAdvance;
   rstl::optional_object< CPreAdvanceIndicator > mSingleAdvance;
 };
+
 class IMetaAnim {
 public:
   virtual ~IMetaAnim() {}
+
   virtual rstl::rc_ptr< CAnimTreeNode >
   GetAnimationTree(const CAnimSysContext& animSys, const CMetaAnimTreeBuildOrders& orders) const;
   virtual void GetUniquePrimitives(rstl::set< CPrimitive >& primsOut) const = 0;
@@ -48,8 +60,11 @@ public:
   virtual rstl::rc_ptr< CAnimTreeNode >
   VGetAnimationTree(const CAnimSysContext& animSys,
                     const CMetaAnimTreeBuildOrders& orders) const = 0;
+
   void PutTo(COutputStream&) const;
+
   static void AdvanceAnim(IAnimReader& anim, const CCharAnimTime& dt);
   static CCharAnimTime GetTime(const CPreAdvanceIndicator& ind, const IAnimReader& anim);
 };
+
 #endif // _IMETAANIM
