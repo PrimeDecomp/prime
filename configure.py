ac63c2ae67bb3449d175ba060c7c9179b33c0655
Phillip Stephens <antidote.crk@gmail.com>
2026-07-27 13:29:14 -0700

    Unmatch OSFatal for GM8E01_01

--- configure.py
+++ configure.py
@@ -1938,7 +1938,7 @@ config.libs = [
                 MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48"),
                 "dolphin/os/OSError.c",
             ),
-            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "dolphin/os/OSFatal.c"),
+            Object(MatchingFor("GM8E01_00"), "dolphin/os/OSFatal.c"),
             Object(NonMatching, "dolphin/os/OSFont.c"),
             Object(
                 MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48"),
