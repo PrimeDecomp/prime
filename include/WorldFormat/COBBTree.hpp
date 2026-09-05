8f90f154f9b89d93f83240c62248e0c9b11aebd6
Luke Street <luke@street.dev>
2026-09-05 15:50:54 -0600

    Restore collider interfaces

--- include/WorldFormat/COBBTree.hpp
+++ include/WorldFormat/COBBTree.hpp
@@ -1,14 +1,77 @@
 #ifndef _COBBTREE
 #define _COBBTREE
 
+#include "types.h"
+
+#include "Collision/COBBox.hpp"
 #include "Kyoto/Math/CVector3f.hpp"
 
 #include "WorldFormat/CCollisionEdge.hpp"
 
 #include "rstl/vector.hpp"
 
+class CAABox;
+class CCollisionSurface;
+class CInputStream;
+class CTransform4f;
+
 class COBBTree {
-  class CNode {};
+public:
+  class CSimpleAllocator {
+    void* x0_buffer;
+    uint x4_size;
+    uint x8_offset;
+
+  public:
+    CSimpleAllocator(uint size);
+    ~CSimpleAllocator();
+    void* Alloc(size_t size);
+  };
+
+  class CLeafData {
+    rstl::vector< ushort > x0_surface;
+
+  public:
+    CLeafData() {}
+    CLeafData(const rstl::vector< ushort >& surface);
+    CLeafData(CInputStream& in);
+
+    const rstl::vector< ushort >& GetSurfaceVector() const { return x0_surface; }
+    uint GetMemoryUsage() const;
+  };
+
+  class CNode {
+    COBBox x0_obb;
+    bool x3c_isLeaf;
+    const CNode* x40_left;
+    const CNode* x44_right;
+    const CLeafData* x48_leaf;
+    mutable bool x4c_hit;
+
+    static CSimpleAllocator* spAllocator;
+
+  public:
+    CNode(const CTransform4f& xf, const CVector3f& extents, const CNode* left, const CNode* right,
+          const CLeafData* leaf);
+    CNode(CInputStream& in);
+    ~CNode();
+
+    const COBBox& GetOBB() const { return x0_obb; }
+    bool IsLeaf() const { return x3c_isLeaf; }
+    const CNode& GetLeftNode() const { return *x40_left; }
+    const CNode& GetRightNode() const { return *x44_right; }
+    const CLeafData& GetLeafData() const { return *x48_leaf; }
+    void SetHit(bool h) const { x4c_hit = h; }
+    bool WasHit() const { return x4c_hit; }
+
+    uint GetMemoryUsage() const;
+
+    static void SetAllocator(CSimpleAllocator* alloc);
+    void* operator new(size_t size, const char* file, int line);
+    void operator delete(void* ptr, size_t size);
+    void operator delete(void* ptr, const char* file, int line);
+  };
+
   struct SIndexData {
     rstl::vector< uint > x0_materials;
     rstl::vector< uchar > x10_vertMaterials;
@@ -17,16 +80,49 @@ class COBBTree {
     rstl::vector< CCollisionEdge > x40_edges;
     rstl::vector< ushort > x50_surfaceIndices;
     rstl::vector< CVector3f > x60_vertices;
+
+    SIndexData();
+    SIndexData(const SIndexData& other);
     SIndexData(CInputStream& in);
   };
 
-public:
   COBBTree(CInputStream& in);
   COBBTree(const SIndexData& indexData, const CNode* root);
   ~COBBTree();
 
+  const CNode& GetRoot() const { return *x88_root; }
+
+  CCollisionSurface GetSurface(ushort idx) const;
+  CCollisionSurface GetTransformedSurface(ushort idx, const CTransform4f& xf) const;
+  const ushort* GetTriangleEdgeIndices(ushort idx) const;
+  void GetTriangleVertexIndices(ushort idx, ushort* out) const;
+
+  const CCollisionEdge& GetEdge(int idx) const { return x18_indexData.x40_edges[idx]; }
+  const CVector3f& GetVert(int idx) const { return x18_indexData.x60_vertices[idx]; }
+  uint GetVertMaterial(int idx) const {
+    return x18_indexData.x0_materials[x18_indexData.x10_vertMaterials[idx]];
+  }
+  uint GetEdgeMaterial(int idx) const {
+    return x18_indexData.x0_materials[x18_indexData.x20_edgeMaterials[idx]];
+  }
+
+  CAABox CalculateLocalAABox() const;
+
+  static COBBTree* BuildOrientedBoundingBoxTree(const CVector3f& extent, const CVector3f& center);
+
 private:
-  char data[0x8c];
+  uint x0_magic;
+  uint x4_version;
+  uint x8_memsize;
+  CSimpleAllocator xc_allocator;
+  SIndexData x18_indexData;
+  const CNode* x88_root;
 };
+
+CHECK_SIZEOF(COBBTree, 0x8c)
+NESTED_CHECK_SIZEOF(COBBTree, CSimpleAllocator, 0xc)
+NESTED_CHECK_SIZEOF(COBBTree, CLeafData, 0x10)
+NESTED_CHECK_SIZEOF(COBBTree, CNode, 0x50)
+NESTED_CHECK_SIZEOF(COBBTree, SIndexData, 0x70)
 
 #endif // _COBBTREE
--- src/WorldFormat/CAreaOctTree.cpp
+++ src/WorldFormat/CAreaOctTree.cpp
@@ -1,7 +1,10 @@
 #include "WorldFormat/CAreaOctTree.hpp"
-#include "Kyoto/Basics/CBasics.hpp"
 #include "Kyoto/Math/CAABox.hpp"
 #include "Kyoto/Streams/CMemoryInStream.hpp"
+
+const ushort* CAreaOctTree::GetTriangleEdgeIndices(ushort idx) const {
+  return &x44_polyEdges[idx * 3];
+}
 
 static CAABox BoxFromIndex(int index, const CVector3f& a, const CVector3f& b, const CVector3f& c) {
   switch (index) {
@@ -27,15 +30,16 @@ static CAABox BoxFromIndex(int index, const CVector3f& a, const CVector3f& b, co
 }
 
 CAreaOctTree::Node CAreaOctTree::Node::GetChild(int index) const {
-  const ushort* node = reinterpret_cast< const ushort* >(x18_ptr + (index * 4) + 4);
   ETreeType type = GetChildType(index);
+  const uint* offsets = reinterpret_cast< const uint* >(x18_ptr + sizeof(uint));
+  const void* node = x18_ptr + 9 * sizeof(uint) + offsets[index];
   if (type == kTT_Leaf) {
-    CAABox bounds = reinterpret_cast< const Node* >(node)->GetBoundingBox();
-    return Node(reinterpret_cast< const uchar* >(node), bounds, GetOwner(), type);
+    CAABox bounds = *reinterpret_cast< const CAABox* >(node);
+    return Node(node, bounds, GetOwner(), type);
   }
-  CAABox bounds =
-      BoxFromIndex(index, x0_aabb.GetMinPoint(), x0_aabb.CenterPoint(), x0_aabb.GetMaxPoint());
-  return Node(reinterpret_cast< const uchar* >(node), bounds, GetOwner(), type);
+  const CVector3f center = 0.5f * (x0_aabb.GetMinPoint() + x0_aabb.GetMaxPoint());
+  CAABox bounds = BoxFromIndex(index, x0_aabb.GetMinPoint(), center, x0_aabb.GetMaxPoint());
+  return Node(node, bounds, GetOwner(), type);
 }
 
 CAreaOctTree::TriListReference CAreaOctTree::Node::GetTriangleArray() const {
@@ -44,7 +48,7 @@ CAreaOctTree::TriListReference CAreaOctTree::Node::GetTriangleArray() const {
     return TriListReference(skDeadArray);
   }
 
-  return TriListReference(reinterpret_cast< const ushort* >(x18_ptr));
+  return TriListReference(x18_ptr);
 }
 
 CAreaOctTree::CAreaOctTree(const CAABox& bounds, Node::ETreeType treeType, uchar* buf,
@@ -71,26 +75,36 @@ CAreaOctTree::CAreaOctTree(const CAABox& bounds, Node::ETreeType treeType, uchar
 void CAreaOctTree::MakeFromMemory(void* buf, const uint bufLen, CAreaOctTree** treeOut,
                                   bool* valid) {
   CMemoryInStream in(buf, bufLen, CMemoryInStream::kOS_NotOwned);
-  in.Get< int >();
-  in.Get< int >();
+  in.ReadLong();
+  in.ReadLong();
   CAABox bounds(in);
-  Node::ETreeType treeType = static_cast< Node::ETreeType >(in.Get< int >());
-  uint treeSize = in.Get< uint >();
+  Node::ETreeType treeType = static_cast< Node::ETreeType >(in.ReadLong());
+  uint treeSize = in.ReadLong();
 
-  uchar* curBuf = reinterpret_cast< uchar* >(buf) + in.GetReadPosition() + treeSize;
-  uint matCount = CBasics::SwapBytes(*reinterpret_cast< uint* >(curBuf));
-  curBuf += sizeof(uint) * matCount;
-  curBuf += 4;
-  uint* matBuf = reinterpret_cast< uint* >(curBuf);
-  curBuf += 4;
-  uchar* vertexMaterials = curBuf;
-  curBuf += 4;
-  uchar* edgeMaterials = curBuf;
-  curBuf += 4;
-  uchar* polyMaterials = curBuf;
-  *treeOut = rs_new CAreaOctTree(bounds, treeType, reinterpret_cast< uchar* >(buf),
-                                 reinterpret_cast< uchar* >(buf) + in.GetReadPosition(), matCount,
-                                 reinterpret_cast< uint* >(matBuf), vertexMaterials, edgeMaterials,
-                                 polyMaterials, 0, nullptr, 0, nullptr, 0, nullptr);
+  uchar* treeBuf = static_cast< uchar* >(buf) + in.GetReadPosition();
+  uint* materialHeader = reinterpret_cast< uint* >(treeBuf + treeSize);
+  uint matCount = *materialHeader;
+  uint* materials = materialHeader + 1;
+
+  uint* vertexMaterialHeader = materials + matCount;
+  uchar* vertexMaterials = reinterpret_cast< uchar* >(vertexMaterialHeader + 1);
+  uint* edgeMaterialHeader = reinterpret_cast< uint* >(vertexMaterials + *vertexMaterialHeader);
+  uchar* edgeMaterials = reinterpret_cast< uchar* >(edgeMaterialHeader + 1);
+  uint* polyMaterialHeader = reinterpret_cast< uint* >(edgeMaterials + *edgeMaterialHeader);
+  uchar* polyMaterials = reinterpret_cast< uchar* >(polyMaterialHeader + 1);
+
+  uint* edgeHeader = reinterpret_cast< uint* >(polyMaterials + *polyMaterialHeader);
+  uint edgeCount = *edgeHeader;
+  CCollisionEdge* edges = reinterpret_cast< CCollisionEdge* >(edgeHeader + 1);
+  uint* polyHeader = reinterpret_cast< uint* >(edges + edgeCount);
+  uint polyCount = *polyHeader / 3;
+  ushort* polyEdges = reinterpret_cast< ushort* >(polyHeader + 1);
+  uint* vertexHeader = reinterpret_cast< uint* >(polyEdges + polyCount * 3);
+  uint vertexCount = *vertexHeader;
+  CVector3f* vertices = reinterpret_cast< CVector3f* >(vertexHeader + 1);
+
+  *treeOut = rs_new CAreaOctTree(bounds, treeType, static_cast< uchar* >(buf), treeBuf, matCount,
+                                materials, vertexMaterials, edgeMaterials, polyMaterials,
+                                edgeCount, edges, polyCount, polyEdges, vertexCount, vertices);
   *valid = true;
 }
