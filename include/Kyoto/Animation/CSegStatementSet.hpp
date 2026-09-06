f5c4c23d9873ecdbcc73b07be815aa5b96f69839
Luke Street <luke@street.dev>
2026-09-06 01:19:04 -0600

    CSegStatementSet 99%

--- include/Kyoto/Animation/CSegStatementSet.hpp
+++ include/Kyoto/Animation/CSegStatementSet.hpp
@@ -7,8 +7,8 @@
 
 class CSegStatementSet {
 public:
+  virtual ~CSegStatementSet() = 0;
   CSegStatementSet(void* ptr);
-  inline virtual ~CSegStatementSet() {}
 
   CSegStatement& operator[](const CSegId& id) { return mSegData[id.val()]; }
   const CSegStatement& operator[](const CSegId& id) const { return mSegData[id.val()]; }
@@ -16,11 +16,9 @@ public:
   CSegStatement& Statement(const CSegId& id) { return mSegData[id.val()]; }
   const CSegStatement& GetData(const CSegId& id) const { return mSegData[id.val()]; }
   void Set(const CSegId& id, const CQuaternion& rotation) { Statement(id).Set(rotation); }
-  void Set(const CSegId& id, const CSegStatement& seg);
-  
-  void Set(const CSegId& id, const CVector3f& vec) {
-    mSegData[id.val()].Set(vec);
-  }
+  void Set(const CSegId& id, const CSegStatement& seg) { Statement(id) = seg; }
+
+  void Set(const CSegId& id, const CVector3f& vec) { mSegData[id.val()].Set(vec); }
 
   void Add(const CSegIdList& list, const CCharLayoutInfo& info, const CSegStatementSet& set,
            float weight);
@@ -28,11 +26,15 @@ public:
 protected:
   CSegStatement* mSegData;
 };
+CHECK_SIZEOF(CSegStatementSet, 0x8)
+
+inline CSegStatementSet::~CSegStatementSet() {}
 
 class CStackSegStatementSet : public CSegStatementSet {
 public:
+  ~CStackSegStatementSet() override;
   CStackSegStatementSet();
-  ~CStackSegStatementSet();
 };
+CHECK_SIZEOF(CStackSegStatementSet, 0x8)
 
 #endif // _CSEGSTATEMENTSET
--- src/Kyoto/Animation/CSegStatementSet.cpp
+++ src/Kyoto/Animation/CSegStatementSet.cpp
@@ -1,5 +1,6 @@
 #include "Kyoto/Animation/CSegStatementSet.hpp"
 #include "Kyoto/Alloc/CMemory.hpp"
+#include "Kyoto/Animation/CSegIdList.hpp"
 #include "Kyoto/Animation/CSegStatement.hpp"
 #include <dolphin/os/OSCache.h>
 
@@ -20,18 +21,40 @@ inline void* AllocateSegment() {
 
   return CMemory::Alloc(0xc80);
 }
-inline void FreeSegment(CSegStatement* seg) {}
+inline void FreeSegment(CSegStatement* seg) {
+  char* base = static_cast< char* >(LCGetBase());
+  char* ptr = reinterpret_cast< char* >(seg);
+  if (ptr >= base && ptr < base + 5 * 0xc80) {
+    int index = (ptr - base) / 0xc80;
+    sFreeSegments |= 1 << index;
+  } else {
+    CMemory::Free(seg);
+  }
+}
 } // namespace
 
 CSegStatementSet::CSegStatementSet(void* ptr) : mSegData(static_cast< CSegStatement* >(ptr)) {
-  int i = 100;
-  while (i) {
-    mSegData[i].mStatus = 0;
-    i--;
+  for (int i = 0; i < 100; ++i) {
+    new (&mSegData[i]) CSegStatement;
+  }
+}
+
+void CSegStatementSet::Add(const CSegIdList& list, const CCharLayoutInfo& info,
+                           const CSegStatementSet& set, float weight) {
+  const int count = list.size();
+  for (int i = 0; i < count; ++i) {
+    const CSegId& id = list[i];
+    CSegStatement result;
+    const CSegStatement& prior = GetData(id);
+    const CSegStatement& other = set.GetData(id);
+    result.Set(prior.Orientation() *
+               CQuaternion::Slerp(CQuaternion::NoRotation(), other.Orientation(), weight));
+    if (other.OffsetValid() && prior.OffsetValid()) {
+      result.Set(prior.Offset() + (other.Offset() - info.GetFromParentUnrotated(id)) * weight);
+    }
+    Set(id, result);
   }
 }
 
 CStackSegStatementSet::CStackSegStatementSet() : CSegStatementSet(AllocateSegment()) {}
-CStackSegStatementSet::~CStackSegStatementSet() {
-  FreeSegment(reinterpret_cast< CSegStatement* >(mSegData));
-}
+CStackSegStatementSet::~CStackSegStatementSet() { FreeSegment(mSegData); }
