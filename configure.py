31d8d735314b41ef2654f95cdce0bdf1253e0e74
Luke Street <luke@street.dev>
2026-09-05 21:28:47 -0600

    Match & link CMetaAnimRandom

--- configure.py
+++ configure.py
@@ -1324,7 +1324,7 @@ config.libs = [
             ),
             Object(NonMatching, "Kyoto/Animation/CMetaAnimPhaseBlend.cpp"),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Animation/CMetaAnimPlay.cpp"),
-            Object(NonMatching, "Kyoto/Animation/CMetaAnimRandom.cpp"),
+            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Animation/CMetaAnimRandom.cpp"),
             Object(NonMatching, "Kyoto/Animation/CMetaAnimSequence.cpp"),
             Object(
                 MatchingFor("GM8E01_00", "GM8E01_01"),
