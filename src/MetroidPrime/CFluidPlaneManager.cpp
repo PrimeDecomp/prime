97860fa47f015c89641202cad11a9e6681b7d6f9
Luke Street <luke@street.dev>
2026-09-06 17:47:44 -0600

    CFluidPlaneManager 99%

--- src/MetroidPrime/CFluidPlaneManager.cpp
+++ src/MetroidPrime/CFluidPlaneManager.cpp
@@ -28,14 +28,127 @@ const bool gkWaterBumpMapping = true;
 const int gkWaterEnvMap = 1;
 const bool gkWaterFog = true;
 
-static int kMinValue = 0;
-static int kMaxValue = 255;
+CFluidPlaneManager::CFluidProfile CFluidPlaneManager::sProfile;
 
 uchar sRippleValues[64][64];
 uchar sRippleMins[64];
 uchar sRippleMaxs[64];
 
-uint CFluidPlaneManager::GetFreqTableIndex(float) { return 0; }
+const float CFluidPlaneManager::kOldestTime = 9999.f;
+
+void CFluidPlaneManager::CFluidProfile::Clear() {
+  x10_ = 0.f;
+  xc_ = 0.f;
+  x8_ = 0.f;
+  x4_ = 0.f;
+  x0_ = 0.f;
+}
+
+CFluidPlaneManager::CFluidPlaneManager()
+: x0_rippleManager(20, 0.5f), x11c_uvT(0.f), x120_(false), x121_(false) {
+  sProfile.Clear();
+  for (rstl::reserved_vector< CSplashRecord, 32 >::iterator it = x18_splashes.begin();
+       it != x18_splashes.end(); ++it) {
+    it->SetTime(9999.f);
+  }
+  SetupRippleMap();
+}
+
+void CFluidPlaneManager::Update(float dt) {
+  x11c_uvT = dt + x11c_uvT;
+  x0_rippleManager.Update(dt);
+  for (CSplashRecord* it = SplashRecords().begin(); it != SplashRecords().end(); ++it) {
+    it->SetTime(dt + it->GetTime());
+    if (it->GetTime() > 9999.f) {
+      it->SetTime(9999.f);
+    }
+  }
+}
+
+void CFluidPlaneManager::StartFrame(bool b) const {
+  x121_ = b;
+  sProfile.Clear();
+}
+
+void CFluidPlaneManager::EndFrame() const { x121_ = false; }
+
+float CFluidPlaneManager::GetLastSplashDeltaTime(TUniqueId splasher) const {
+  float newestTime = 9999.f;
+  for (const CSplashRecord* it = GetSplashRecords().begin(); it != GetSplashRecords().end(); ++it) {
+    if (splasher == it->GetUniqueId() && newestTime > it->GetTime()) {
+      newestTime = it->GetTime();
+    }
+  }
+  return newestTime;
+}
+
+float CFluidPlaneManager::GetLastRippleDeltaTime(TUniqueId rippler) const {
+  return x0_rippleManager.GetLastRippleDeltaTime(rippler);
+}
+
+void CFluidPlaneManager::CreateSplash(TUniqueId splasher, CStateManager& mgr,
+                                      const CScriptWater& water, const CVector3f& pos, float factor,
+                                      bool sfx) {
+  if (!water.CanRippleAtPoint(pos)) {
+    return;
+  }
+
+  float oldestTime = 0.f;
+  CSplashRecord* oldestRecord = nullptr;
+  for (CSplashRecord* it = SplashRecords().begin(); it != SplashRecords().end(); ++it) {
+    if (it->GetTime() > oldestTime) {
+      oldestRecord = it;
+      oldestTime = it->GetTime();
+    }
+  }
+
+  CSplashRecord newRecord(splasher);
+  if (oldestRecord != nullptr) {
+    *oldestRecord = newRecord;
+  } else {
+    SplashRecords().push_back(newRecord);
+  }
+
+  float splashScale = water.GetSplashEffectScale(factor);
+  if (water.GetSplashEffect(factor)) {
+    CEntity* expl = rs_new CExplosion(
+            *water.GetSplashEffect(factor), mgr.AllocateUniqueId(), true,
+            CEntityInfo(water.GetCurrentAreaId(), CEntity::NullConnectionList),
+            rstl::string_l("Splash"), CTransform4f(CMatrix3f::Identity(), pos), 1,
+            CVector3f(splashScale, splashScale, splashScale), water.GetSplashColor());
+    if (expl) {
+      mgr.AddObject(*expl);
+    }
+  }
+
+  if (sfx) {
+    CSfxManager::AddEmitter(water.GetSplashSound(factor), pos, CVector3f(0.f, 0.f, 1.f), true,
+                            false, CSfxManager::kMedPriority, CSfxManager::kAllAreas);
+  }
+}
+
+static inline float ComputeRippleAmplitude(float curX, float curY) {
+  float rVal = 1.f - curY;
+  float minX = curY;
+  float maxX = 1.25f * (0.25f * rVal + 0.1f) + curY;
+  if (curY < 0.f) {
+    minX = 0.f;
+  } else if (maxX > 1.f) {
+    maxX = 1.f;
+  }
+  float val = 0.f;
+  if (curX >= minX && curX <= maxX) {
+    float t = (curX - minX) / (maxX - minX);
+    if (t < 0.4f) {
+      val = 2.5f * t;
+    } else if (t > 0.75f) {
+      val = 4.f * (1.f - t);
+    } else {
+      val = 1.f;
+    }
+  }
+  return val;
+}
 
 void CFluidPlaneManager::SetupRippleMap() {
   float curX = 0.f;
@@ -45,26 +158,8 @@ void CFluidPlaneManager::SetupRippleMap() {
     float minY = 1.f;
     float maxY = curY;
     for (int j = 64; j > 0; --j) {
-      float rVal = 1.f - curY;
-      float minX = curY;
-      float maxX = 1.25f * (0.25f * rVal + 0.1f) + curY;
-      if (curY < 0.f) {
-        minX = 0.f;
-      } else if (maxX > 1.f) {
-        maxX = 1.f;
-      }
-      float val = 0.f;
-      if (curX >= minX && curX <= maxX) {
-        float t = (curX - minX) / (maxX - minX);
-        if (t < 0.4f) {
-          val = 2.5f * t;
-        } else if (t > 0.75f) {
-          val = 4.f * (1.f - t);
-        } else {
-          val = 1.f;
-        }
-      }
-      uchar valA = CCast::ToUint8(255.f * (val * (rVal * rVal)));
+      float val = ComputeRippleAmplitude(curX, curY);
+      uchar valA = CCast::ToUint8(255.f * (val * ((1.f - curY) * (1.f - curY))));
       *rowPtr = valA;
       if (valA != 0 && curY < minY) {
         minY = curY;
@@ -76,12 +171,14 @@ void CFluidPlaneManager::SetupRippleMap() {
       rowPtr++;
     }
     int valB = static_cast< int >(CCast::ToUint8(255.f * minY)) - 1;
-    sRippleMins[i] = static_cast< uchar >(rstl::max_val(kMinValue, valB));
+    sRippleMins[i] = static_cast< uchar >(rstl::max_val(0, valB));
     int valC = static_cast< int >(CCast::ToUint8(255.f * maxY)) + 1;
-    sRippleMaxs[i] = static_cast< uchar >(rstl::min_val(kMaxValue, valC));
+    sRippleMaxs[i] = static_cast< uchar >(rstl::min_val(255, valC));
     curX += (1.f / 63.f);
   }
 }
+
+uint CFluidPlaneManager::GetFreqTableIndex(float) { return 0; }
 
 CFluidPlane::CFluidPlane(const CAssetId texPattern1, const CAssetId texPattern2,
                          const CAssetId texColor, const float alpha, const EFluidType fluidType,
@@ -173,103 +270,3 @@ void CFluidPlane::AddRipple(const CRipple& ripple, const CScriptWater& water, CS
 
 void CFluidPlane::Render(const CStateManager& mgr, const CAABox&, const CFrustumPlanes&,
                          const CRippleManager&, const CVector3f&) {}
-
-// void CFluidPlane::Render(const CStateManager&, float, const CAABox&, const CTransform4f&,
-//                          const CTransform4f&, bool, const CFrustumPlanes&,
-//                          const rstl::optional_object< CRippleManager >&, TUniqueId,
-//                          const bool*, int, int, const CVector3f&) const {}
-
-void CFluidPlaneManager::CreateSplash(TUniqueId splasher, CStateManager& mgr,
-                                      const CScriptWater& water, const CVector3f& pos, float factor,
-                                      bool sfx) {
-  if (!water.CanRippleAtPoint(pos)) {
-    return;
-  }
-
-  float oldestTime = 0.f;
-  CSplashRecord* oldestRecord = nullptr;
-  for (CSplashRecord* it = SplashRecords().begin(); it != SplashRecords().end(); ++it) {
-    if (it->GetTime() > oldestTime) {
-      oldestRecord = it;
-      oldestTime = it->GetTime();
-    }
-  }
-
-  CSplashRecord newRecord(splasher);
-  if (oldestRecord != nullptr) {
-    *oldestRecord = newRecord;
-  } else {
-    SplashRecords().push_back(newRecord);
-  }
-
-  float splashScale = water.GetSplashEffectScale(factor);
-  if (water.GetSplashEffect(factor)) {
-    CEntity* expl = rs_new CExplosion(
-            *water.GetSplashEffect(factor), mgr.AllocateUniqueId(), true,
-            CEntityInfo(water.GetCurrentAreaId(), CEntity::NullConnectionList),
-            rstl::string_l("Splash"), CTransform4f(CMatrix3f::Identity(), pos), 1,
-            CVector3f(splashScale, splashScale, splashScale), water.GetSplashColor());
-    if (expl) {
-      mgr.AddObject(*expl);
-    }
-  }
-
-  if (sfx) {
-    CSfxManager::AddEmitter(water.GetSplashSound(factor), pos, CVector3f(0.f, 0.f, 1.f), true,
-                            false, CSfxManager::kMedPriority, CSfxManager::kAllAreas);
-  }
-}
-
-float CFluidPlaneManager::GetLastRippleDeltaTime(TUniqueId rippler) const {
-  return x0_rippleManager.GetLastRippleDeltaTime(rippler);
-}
-
-const float CFluidPlaneManager::kOldestTime = 9999.f;
-
-float CFluidPlaneManager::GetLastSplashDeltaTime(TUniqueId splasher) const {
-  float newestTime = 9999.f;
-  for (const CSplashRecord* it = GetSplashRecords().begin(); it != GetSplashRecords().end(); ++it) {
-    if (splasher == it->GetUniqueId() && newestTime > it->GetTime()) {
-      newestTime = it->GetTime();
-    }
-  }
-  return newestTime;
-}
-
-void CFluidPlaneManager::EndFrame() const { x121_ = false; }
-
-void CFluidPlaneManager::StartFrame(bool b) const {
-  x121_ = b;
-  sProfile.Clear();
-}
-
-void CFluidPlaneManager::Update(float dt) {
-  x11c_uvT = dt + x11c_uvT;
-  x0_rippleManager.Update(dt);
-  for (CSplashRecord* it = SplashRecords().begin(); it != SplashRecords().end(); ++it) {
-    it->SetTime(dt + it->GetTime());
-    if (it->GetTime() > 9999.f) {
-      it->SetTime(9999.f);
-    }
-  }
-}
-
-CFluidPlaneManager::CFluidProfile CFluidPlaneManager::sProfile;
-
-CFluidPlaneManager::CFluidPlaneManager()
-: x0_rippleManager(20, 0.5f), x11c_uvT(0.f), x120_(false), x121_(false) {
-  sProfile.Clear();
-  for (rstl::reserved_vector< CSplashRecord, 32 >::iterator it = x18_splashes.begin();
-       it != x18_splashes.end(); ++it) {
-    it->SetTime(9999.f);
-  }
-  SetupRippleMap();
-}
-
-void CFluidPlaneManager::CFluidProfile::Clear() {
-  x10_ = 0.f;
-  xc_ = 0.f;
-  x8_ = 0.f;
-  x4_ = 0.f;
-  x0_ = 0.f;
-}
