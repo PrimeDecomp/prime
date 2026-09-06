897d6cb67afc3f7a6d719c89136d7b3b2a92356e
Luke Street <luke@street.dev>
2026-09-05 19:53:47 -0600

    Match & link CAnimTreeTimeScale

--- configure.py
+++ configure.py
@@ -1381,7 +1381,7 @@ config.libs = [
                 MatchingFor("GM8E01_00", "GM8E01_01"),
                 "Kyoto/Animation/CAnimTreeSingleChild.cpp",
             ),
-            Object(NonMatching, "Kyoto/Animation/CAnimTreeTimeScale.cpp"),
+            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Animation/CAnimTreeTimeScale.cpp"),
             Object(NonMatching, "Kyoto/Animation/CAnimTreeTransition.cpp"),
             Object(NonMatching, "Kyoto/Animation/CAnimTreeTweenBase.cpp"),
             Object(
