8c9fb9c54eafe4c1af14329af2905c6a06e326e3
Luke Street <luke@street.dev>
2026-09-06 05:44:51 -0600

    Match & link CWordBreakTables

--- configure.py
+++ configure.py
@@ -1461,7 +1461,7 @@ config.libs = [
                 MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Text/CTextInstruction.cpp"
             ),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Text/CTextParser.cpp"),
-            Object(NonMatching, "Kyoto/Text/CWordBreakTables.cpp"),
+            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Text/CWordBreakTables.cpp"),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "Kyoto/Text/CWordInstruction.cpp"),
             Object(
                 MatchingFor("GM8E01_00", "GM8E01_01"),
