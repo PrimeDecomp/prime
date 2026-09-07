3fb2167ba9161b8067f53620e72ad34c65a8aeb3
Luke Street <luke@street.dev>
2026-09-07 16:59:19 -0600

    Match & link CKnockBackMgr (#167)

--- configure.py
+++ configure.py
@@ -1075,7 +1075,7 @@ config.libs = [
                 MatchingFor("GM8E01_00", "GM8E01_01"),
                 "MetroidPrime/Enemies/CRipperControlledPlatform.cpp",
             ),
-            Object(NonMatching, "MetroidPrime/Enemies/CKnockBackMgr.cpp"),
+            Object(Matching, "MetroidPrime/Enemies/CKnockBackMgr.cpp"),
             Object(Matching, "MetroidPrime/CScriptLayerManager.cpp"),
             Object(NonMatching, "MetroidPrime/Enemies/CMagdolite.cpp"),
             Object(NonMatching, "MetroidPrime/Enemies/CTeamAiMgr.cpp"),
