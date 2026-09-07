483b880fc8250675595295c845b6a3bdf5df1a15
Luke Street <luke@street.dev>
2026-09-06 22:27:59 -0600

    CHudBallInterface 97% (#126)

--- include/MetroidPrime/HUD/CHudBallInterface.hpp
+++ include/MetroidPrime/HUD/CHudBallInterface.hpp
@@ -12,19 +12,20 @@ class CGuiWidget;
 
 class CHudBallInterface {
 public:
-  CHudBallInterface(CGuiFrame& selHud, int pbAmount, int pbCapacity, int availableBombs, bool hasBombs, bool hasPb);
-  void SetBombParams(int pbAmount, int pbCapacity, int availableBombs, bool hasBombs, bool hasPb, bool init);
+  CHudBallInterface(CGuiFrame& selHud, int pbAmount, int pbCapacity, int availableBombs,
+                    bool hasBombs, bool hasPb);
+  void SetBombParams(int pbAmount, int pbCapacity, int availableBombs, bool hasBombs, bool hasPb,
+                     bool init);
   void SetBallModeFactor(float t);
 
-  private:
-  
+private:
   CGuiCamera* x0_camera;
   CGuiWidget* x4_basewidget_bombstuff;
   CGuiWidget* x8_basewidget_bombdeco;
   CGuiModel* xc_model_bombicon;
   CGuiTextPane* x10_textpane_bombdigits;
-  rstl::reserved_vector<CGuiWidget*, 3> x14_group_bombfilled;
-  rstl::reserved_vector<CGuiWidget*, 3> x24_group_bombempty;
+  rstl::reserved_vector< CGuiWidget*, 3 > x14_group_bombfilled;
+  rstl::reserved_vector< CGuiWidget*, 3 > x24_group_bombempty;
   CVector3f x34_camPos;
   int x40_pbAmount;
   int x44_pbCapacity;
@@ -32,5 +33,6 @@ public:
   bool x4c_hasPb;
   void UpdatePowerBombReadoutColors();
 };
+CHECK_SIZEOF(CHudBallInterface, 0x50)
 
 #endif // _CHUDBALLINTERFACE
