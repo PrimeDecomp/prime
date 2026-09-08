b2d7713a42c8e44e48cb171a701aa705d44e8311
Luke Street <luke@street.dev>
2026-09-08 09:58:47 -0600

    CSaveGameScreen 99%

--- include/MetroidPrime/CMain.hpp
+++ include/MetroidPrime/CMain.hpp
@@ -81,6 +81,7 @@ public:
   void SetRestartMode(const ERestartMode s) { x12c_restartMode = s; }
   ERestartMode GetRestartMode() const { return x12c_restartMode; }
   void SetCardBusy(bool v) { x160_31_cardBusy = v; }
+  void SetManageCard(bool v) { x160_28_manageCard = v; }
   bool GetCardBusy() const { return x160_31_cardBusy; }
 
   void SetMaxSpeed(bool v) {
--- include/MetroidPrime/CSaveGameScreen.hpp
+++ include/MetroidPrime/CSaveGameScreen.hpp
@@ -1,9 +1,20 @@
 #ifndef _CSAVEGAMESCREEN
 #define _CSAVEGAMESCREEN
 
+#include "Kyoto/TToken.hpp"
+#include "MetroidPrime/CIOWin.hpp"
+#include "MetroidPrime/CMemoryCardDriver.hpp"
+#include "rstl/single_ptr.hpp"
+#include "rstl/vector.hpp"
 #include "types.h"
 
 class CFinalInput;
+class CTexture;
+class CWorldSaveGameInfo;
+class CStringTable;
+class CGuiFrame;
+class CGuiTextPane;
+class CGuiTableGroup;
 
 enum ESaveContext { kSC_FrontEnd, kSC_InGame };
 
@@ -31,28 +42,54 @@ public:
 
   CSaveGameScreen(ESaveContext saveContext, u64 cardSerial);
   ~CSaveGameScreen();
-  int Update(float dt);
+  CIOWin::EMessageReturn Update(float dt);
   bool PumpLoad();
 
   EUIType GetUIType() const { return x10_uiType; }
   bool IsSavingDisabled() const { return x92_savingDisabled; }
-  const void* GetGameData(int idx);
+  const CGameState::GameFileStateInfo* GetGameData(int idx) const;
   void EraseGame(int idx);
   void StartGame(int idx);
   void ResetCardDriver();
   void ProcessUserInput(const CFinalInput& input);
-  void Draw();
+  void Draw() const;
   static bool IsHiddenFromFrontEnd(EUIType type);
   void SetInGame(bool inGame);
   void SaveNESState();
+  static CMemoryCardDriver* ConstructCardDriver(bool importPersistent);
+  EUIType SelectUIType() const;
+  void SetUIText();
+  void SetUIColors();
+  void DoAdvance(CGuiTableGroup* caller);
+  void DoSelectionChange(CGuiTableGroup* caller, int oldSelection);
 
 private:
-  uchar x0_pad[0x10];
+  void ContinueWithoutSaving();
+  ESaveContext x0_saveCtx;
+  u64 x8_serial;
   EUIType x10_uiType;
-  uchar x14_pad[0x92 - 0x14];
+  TCachedToken< CTexture > x14_txtrSaveBanner;
+  TCachedToken< CTexture > x20_txtrSaveIcon0;
+  TCachedToken< CTexture > x2c_txtrSaveIcon1;
+  TCachedToken< CStringTable > x38_strgMemoryCard;
+  TCachedToken< CGuiFrame > x44_frmeGenericMenu;
+  CGuiFrame* x50_loadedFrame;
+  CGuiTextPane* x54_textpane_message;
+  CGuiTableGroup* x58_tablegroup_choices;
+  CGuiTextPane* x5c_textpane_choice0;
+  CGuiTextPane* x60_textpane_choice1;
+  CGuiTextPane* x64_textpane_choice2;
+  CGuiTextPane* x68_textpane_choice3;
+  rstl::single_ptr< CMemoryCardDriver > x6c_cardDriver;
+  rstl::vector< TToken< CWorldSaveGameInfo > > x70_saveWorlds;
+  CIOWin::EMessageReturn x80_iowRet;
+  uint x84_navConfirmSfx;
+  uint x88_navMoveSfx;
+  uint x8c_navBackSfx;
+  bool x90_needsDriverReset;
+  bool x91_uiTextDirty;
   bool x92_savingDisabled;
   bool x93_inGame;
-  uchar x94_pad[0x98 - 0x94];
 };
 CHECK_SIZEOF(CSaveGameScreen, 0x98)
 
