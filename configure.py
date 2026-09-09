c1606f52ce99adb36700f6e18d4a03dae27a5e31
Luke Street <luke@street.dev>
2026-09-08 21:31:25 -0600

    Match & link OSFont and OSReboot

--- configure.py
+++ configure.py
@@ -1980,7 +1980,7 @@ config.libs = [
                 "dolphin/os/OSError.c",
             ),
             Object(MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_02"), "dolphin/os/OSFatal.c"),
-            Object(NonMatching, "dolphin/os/OSFont.c"),
+            Object(Matching, "dolphin/os/OSFont.c"),
             Object(
                 MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48", "GM8E01_02"),
                 "dolphin/os/OSInterrupt.c",
@@ -2001,7 +2001,7 @@ config.libs = [
                 MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48", "GM8E01_02"),
                 "dolphin/os/OSMutex.c",
             ),
-            Object(NonMatching, "dolphin/os/OSReboot.c"),
+            Object(MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48"), "dolphin/os/OSReboot.c"),
             Object(
                 MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48", "GM8E01_02"),
                 "dolphin/os/OSReset.c",
