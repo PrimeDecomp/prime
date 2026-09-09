4aea993a5668d7f1720c1b17022b46523133afe5
Yury <shulkmaster@gmail.com>
2026-09-08 19:53:19 -0600

    Match & link CEnergyBall (#256)
    * Start CEnergyBall
    
    * Match & link CEnergyBall

--- configure.py
+++ configure.py
@@ -1174,7 +1174,7 @@ config.libs = [
             Object(
                 NonMatching, "MetroidPrime/ScriptObjects/CScriptProjectedShadow.cpp"
             ),
-            Object(NonMatching, "MetroidPrime/ScriptObjects/CEnergyBall.cpp"),
+            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/ScriptObjects/CEnergyBall.cpp"),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/ScriptObjects/CSustainedPlayerDamage.cpp"),
             Object(
                 MatchingFor("GM8E01_00", "GM8E01_01"),
--- src/MetroidPrime/ScriptObjects/CEnergyBall.cpp
+++ src/MetroidPrime/ScriptObjects/CEnergyBall.cpp
@@ -1,6 +1,15 @@
 #include "MetroidPrime/ScriptObjects/CEnergyBall.hpp"
+#include "Kyoto/Audio/CSfxManager.hpp"
+#include "MetroidPrime/BodyState/CBodyController.hpp"
+#include "MetroidPrime/CAnimData.hpp"
+#include "MetroidPrime/CStateManager.hpp"
+#include "MetroidPrime/Player/CPlayer.hpp"
+#include "MetroidPrime/Player/CPlayerState.hpp"
+#include "MetroidPrime/ScriptObjects/CHUDBillboardEffect.hpp"
 #include "MetroidPrime/ScriptObjects/CSustainedPlayerDamage.hpp"
 
+static EMaterialTypes sSolidMaterial = kMT_Solid;
+static EMaterialTypes sPlayerMaterial = kMT_Player;
 
 CEnergyBall::CEnergyBall(const TUniqueId uid, const rstl::string& name, const CEntityInfo& info,
                          const CTransform4f& xf, const CModelData& mData,
@@ -40,3 +49,132 @@ CEnergyBall::CEnergyBall(const TUniqueId uid, const rstl::string& name, const CE
 }
 
 CEnergyBall::~CEnergyBall() {}
+
+void CEnergyBall::Accept(IVisitor& visitor) { visitor.Visit(*this); }
+
+void CEnergyBall::AcceptScriptMsg(EScriptObjectMessage msg, TUniqueId uid, CStateManager& mgr) {
+  if (msg == kSM_Registered) {
+    SetMaterialFilter(CMaterialFilter::MakeInclude(CMaterialList(sPlayerMaterial)));
+    RemoveMaterial(kMT_Solid, mgr);
+  }
+  CPatterned::AcceptScriptMsg(msg, uid, mgr);
+}
+
+void CEnergyBall::Generate(CStateManager& mgr, EStateMsg msg, float arg) {
+  switch (msg) {
+  case kStateMsg_Activate:
+    x32c_animState = kAS_Ready;
+    // Fall through.
+  case kStateMsg_Update:
+    TryGenerateDeactivate(mgr, 0);
+    if (!BodyCtrl()->GetIsActive()) {
+      BodyCtrl()->Activate(mgr);
+    }
+    break;
+  case kStateMsg_Deactivate:
+    x32c_animState = kAS_NotReady;
+    break;
+  }
+}
+
+void CEnergyBall::Attack(CStateManager& mgr, EStateMsg msg, float arg) {
+  switch (msg) {
+  case kStateMsg_Activate:
+    break;
+  case kStateMsg_Update: {
+    const CVector3f eyePos = mgr.GetPlayer()->GetEyePosition();
+    const CVector3f seek = mSteeringBehaviors.Seek(*this, eyePos);
+    BodyCtrl()->FaceDirection3D(seek, GetTransform().GetForward(), arg);
+    break;
+  }
+  case kStateMsg_Deactivate:
+    break;
+  }
+}
+
+void CEnergyBall::Think(float dt, CStateManager& mgr) {
+  const float turnRatio = (x56c - 2.5f) / 8.f;
+  const float turnSpeed = mInitialTurnSpeed * rstl::max_val(0.f, rstl::min_val(1.f, turnRatio));
+  x3b8_turnSpeed = turnSpeed;
+  BodyCtrl()->SetTurnSpeed(turnSpeed);
+  CPatterned::Think(dt, mgr);
+  const float damageRatio = x428_damageCooldownTimer / skDamageHitTime;
+  ModelData()->AnimationData()->GetParticleDB().SetModulationColorAllActiveEffects(CColor::Lerp(
+      CColor::White(), CColor::Red(), rstl::max_val(0.f, rstl::min_val(1.f, damageRatio))));
+
+  bool shouldDetonate = false;
+  if (GetActive() && IsAlive()) {
+    x56c += dt;
+    if (x56c > x574) {
+      shouldDetonate = true;
+    }
+    if (InMaxRange(mgr, dt)) {
+      shouldDetonate = true;
+    }
+  }
+  if (shouldDetonate) {
+    Detonate(mgr);
+  }
+}
+
+void CEnergyBall::Detonate(CStateManager& mgr) {
+  const CVector3f& delta = mgr.GetPlayer()->GetTranslation() - GetTranslation();
+  if (delta.MagSquared() <= x578.GetRadius() * x578.GetRadius()) {
+    bool breakFrozen = true;
+    switch (x570) {
+    case 0: {
+      const float duration = x598;
+      x402_28_isMakingBigStrike = duration > 0.f;
+      x504_damageDur = duration;
+      break;
+    }
+    case 1: {
+      const ushort freezeSfx = x5a0;
+      mgr.Player()->SetFrozenState(mgr, x59c, freezeSfx, x5a4);
+      breakFrozen = false;
+      break;
+    }
+    case 2:
+      CreateVisorEffect(rstl::optional_object< TToken< CGenDescription > >(), x5a8,
+                        rstl::string_l("PlasmaElectricFx"), mgr);
+      CSfxManager::SfxStart(x5b4, 127, 64, false, CSfxManager::kMedPriority, false,
+                            CSfxManager::kAllAreas);
+      mgr.PlayerState()->StaticInterference().AddSource(GetUniqueId(), x5b8, x5bc);
+      break;
+    case 3:
+      CreateVisorEffect(x5c0, rstl::optional_object< TToken< CElectricDescription > >(),
+                        rstl::string_l("PlasmaVisorFx"), mgr);
+      mgr.AddObject(rs_new CSustainedPlayerDamage(
+          mgr.AllocateUniqueId(),
+          CEntityInfo(GetCurrentAreaId(), CEntity::NullConnectionList, kInvalidEditorId), true,
+          rstl::string_l("SusDamage"), x5cc, x5e8));
+      break;
+    }
+    if (breakFrozen && mgr.GetPlayer()->GetFrozenState()) {
+      mgr.Player()->BreakFrozenState(mgr);
+    }
+  }
+  mgr.ApplyDamageToWorld(
+      GetUniqueId(), *this, GetTranslation(), x578,
+      CMaterialFilter::MakeIncludeExclude(CMaterialList(sSolidMaterial), CMaterialList()));
+  MassiveDeath(mgr);
+}
+
+void CEnergyBall::Death(CStateManager& mgr, const CVector3f& direction, EScriptObjectState state) {
+  CHealthInfo* health = HealthInfo(mgr);
+  if (health && health->GetHP() > 0.f) {
+    CPatterned::Death(mgr, direction, kSS_Any);
+  } else {
+    CPatterned::Death(mgr, direction, kSS_DeathRattle);
+  }
+}
+
+void CEnergyBall::CreateVisorEffect(
+    const rstl::optional_object< TToken< CGenDescription > >& particle,
+    const rstl::optional_object< TToken< CElectricDescription > >& electric,
+    const rstl::string& name, CStateManager& mgr) {
+  mgr.AddObject(rs_new CHUDBillboardEffect(
+      particle, electric, mgr.AllocateUniqueId(), true, name,
+      CHUDBillboardEffect::GetNearClipDistance(mgr), CHUDBillboardEffect::GetScaleForPOV(mgr),
+      CColor(1.f, 1.f, 1.f, 1.f), CVector3f(1.f, 1.f, 1.f), CVector3f(0.f, 0.f, 0.f)));
+}
