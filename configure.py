0708738f0174459a48e594e8d6d570c09990103d
Luke Street <luke@street.dev>
2026-09-08 20:54:09 -0600

    Match & link CGunWeapon

--- configure.py
+++ configure.py
@@ -957,7 +957,7 @@ config.libs = [
                 "MetroidPrime/GameObjectLists.cpp",
             ),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/Weapons/CAuxWeapon.cpp"),
-            Object(NonMatching, "MetroidPrime/Weapons/CGunWeapon.cpp"),
+            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/Weapons/CGunWeapon.cpp"),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/ScriptObjects/CScriptAreaAttributes.cpp"),
             Object(NonMatching, "MetroidPrime/Weapons/CWaveBuster.cpp"),
             Object(NonMatching, "MetroidPrime/Player/CStaticInterference.cpp"),
