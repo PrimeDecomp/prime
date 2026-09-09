696d2f0ba6867ebeaeb0370a8c0ccbee24d4a8de
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-09 04:22:51 +0300

    CSteeringBehaviors 99% (#239)

--- include/MetroidPrime/CSteeringBehaviors.hpp
+++ include/MetroidPrime/CSteeringBehaviors.hpp
@@ -30,19 +30,26 @@ public:
   CVector2f Flee2D(const CPhysicsActor& actor, const CVector2f& v0) const;
   CVector2f Arrival2D(const CPhysicsActor& actor, const CVector2f& v0) const;
   static bool SolveQuadratic(const float a, const float b, const float c, float& xPos, float& xNeg);
-  static bool ProjectLinearIntersection(const CVector3f& v0, float f1, const CVector3f& v1,
-                                        const CVector3f& v2, CVector3f& v3);
-  static bool ProjectLinearIntersection(const CVector3f& v0, float f1, const CVector3f& v1,
-                                        const CVector3f& v2, const CVector3f& v3, CVector3f& v4);
+  static bool SolveCubic(const rstl::reserved_vector< float, 4 >& coefficients,
+                         rstl::reserved_vector< float, 4 >& roots);
+  static bool SolveQuartic(const rstl::reserved_vector< float, 5 >& coefficients,
+                           rstl::reserved_vector< float, 4 >& roots);
+  static bool ProjectLinearIntersection(const CVector3f& origin, float speed,
+                                        const CVector3f& position, const CVector3f& velocity,
+                                        CVector3f& intersection);
+  static bool ProjectLinearIntersection(const CVector3f& origin, float speed,
+                                        const CVector3f& position, const CVector3f& velocity,
+                                        const CVector3f& acceleration, CVector3f& intersection);
 
-  static bool ProjectOrbitalIntersection(const CVector3f& v0, float f1, float f2,
-                                         const CVector3f& v1, const CVector3f& v2,
-                                         const CVector3f& v3, CVector3f& v4);
-  static bool ProjectOrbitalIntersection(const CVector3f& v0, float f1, float f2,
-                                         const CVector3f& v1, const CVector3f& v2,
-                                         const CVector3f& v3, const CVector3f& v4, CVector3f& v5);
+  static bool ProjectOrbitalIntersection(const CVector3f& origin, float speed, float dt,
+                                         const CVector3f& position, const CVector3f& velocity,
+                                         const CVector3f& orbitPoint, CVector3f& intersection);
+  static bool ProjectOrbitalIntersection(const CVector3f& origin, float speed, float dt,
+                                         const CVector3f& position, const CVector3f& velocity,
+                                         const CVector3f& acceleration, const CVector3f& orbitPoint,
+                                         CVector3f& intersection);
 
-  static CVector3f ProjectOrbitalPosition(const CVector3f& pos, const CVector3f& vel,
+  static CVector3f ProjectOrbitalPosition(const CVector3f& position, const CVector3f& velocity,
                                           const CVector3f& orbitPoint, float dt, float preThinkDt);
 
 private:
