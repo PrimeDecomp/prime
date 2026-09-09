af99ad747efde0201568006db4131d1ad391191e
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-09 11:23:06 +0300

    Match CPASDatabase

--- src/Kyoto/Animation/CPASDatabase.cpp
+++ src/Kyoto/Animation/CPASDatabase.cpp
@@ -1,10 +1,10 @@
 #include "Kyoto/Animation/CPASDatabase.hpp"
+#include "Kyoto/Animation/CPASAnimParmData.hpp"
+#include "Kyoto/CRandom16.hpp"
 #include "Kyoto/Streams/CInputStream.hpp"
 #include "rstl/algorithm.hpp"
 
-#pragma inline_max_size(100)
 CPASDatabase::CPASDatabase(CInputStream& in) : x10_defaultState(-1) {
-
   in.Get< int >();
 
   const uint stateCount = in.Get< uint >();
@@ -19,10 +19,44 @@ CPASDatabase::CPASDatabase(CInputStream& in) : x10_defaultState(-1) {
   }
 }
 
-const CPASAnimState* CPASDatabase::GetAnimStateByIndex(int index) const { return &x0_states[index]; }
+rstl::pair< float, int > CPASDatabase::FindBestAnimation(const CPASAnimParmData& data,
+                                                         int ignoreAnim) const {
+  AUTO(it, rstl::binary_find(x0_states.begin(), x0_states.end(), CPASAnimState(data.GetStateId())));
+  if (it != x0_states.end()) {
+    CRandom16 random(0x1234);
+    return it->FindBestAnimation(data.GetAnimParmData(), random, ignoreAnim);
+  }
+  return rstl::pair< float, int >(0.f, -1);
+}
+
+rstl::pair< float, int > CPASDatabase::FindBestAnimation(const CPASAnimParmData& data,
+                                                         CRandom16& random, int ignoreAnim) const {
+  AUTO(it, rstl::binary_find(x0_states.begin(), x0_states.end(), CPASAnimState(data.GetStateId())));
+  if (it != x0_states.end()) {
+    return it->FindBestAnimation(data.GetAnimParmData(), random, ignoreAnim);
+  }
+  return rstl::pair< float, int >(0.f, -1);
+}
+
+size_t CPASDatabase::GetNumAnimStates() const { return x0_states.size(); }
+
+bool CPASDatabase::HasState(int id) const {
+  AUTO(it, rstl::binary_find(x0_states.begin(), x0_states.end(), CPASAnimState(id)));
+  return it != x0_states.end();
+}
+
+const CPASAnimState* CPASDatabase::GetAnimState(int id) const {
+  return rstl::binary_find(x0_states.begin(), x0_states.end(), CPASAnimState(id)).get_pointer();
+}
+
+const CPASAnimState* CPASDatabase::GetAnimStateByIndex(int index) const {
+  return &x0_states[index];
+}
+
 void CPASDatabase::SetDefaultState(int state) { x10_defaultState = state; }
+
 void CPASDatabase::AddAnimState(const CPASAnimState& state) {
   const rstl::vector< CPASAnimState >::iterator it =
-      rstl::binary_find< rstl::vector< CPASAnimState > >(x0_states.begin(), x0_states.end(), state);
+      rstl::lower_bound(x0_states.begin(), x0_states.end(), state);
   x0_states.insert(it, state);
 }
