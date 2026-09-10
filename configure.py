b8827cc85c6791637c509cd25e397c72325a6156
Phillip Stephens <antidote.crk@gmail.com>
2026-09-10 09:43:09 -0700

    Update configure.py

--- configure.py
+++ configure.py
@@ -835,10 +835,8 @@ config.libs = [
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/BodyState/CBSGroundHit.cpp"),
             Object(NonMatching, "MetroidPrime/Enemies/CChozoGhost.cpp"),
             Object(MatchingFor("GM8E01_00"), "MetroidPrime/Enemies/CFireFlea.cpp"),
-            Object(NonMatching, "MetroidPrime/BodyState/CBSSlide.cpp"),
-            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/BodyState/CBSHurled.cpp"),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/BodyState/CBSSlide.cpp"),
-            Object(NonMatching, "MetroidPrime/BodyState/CBSHurled.cpp"),
+            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/BodyState/CBSHurled.cpp"),
             Object(NonMatching, "MetroidPrime/BodyState/CBSJump.cpp"),
             Object(
                 MatchingFor("GM8E01_00", "GM8E01_01"),
