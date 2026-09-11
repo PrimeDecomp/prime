36a951829c2d4030313d75e5c6a7668fa09b2823
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-11 19:22:02 +0300

    Improving CMediumAllocPool for PAL

--- configure.py
+++ configure.py
@@ -1869,7 +1869,16 @@ config.libs = [
                 MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48"),
                 "Kyoto/DolphinCDvdFile.cpp",
             ),
-            Object(MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48"), "Kyoto/Alloc/CMediumAllocPool.cpp"),
+            Object(
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48"), 
+                "Kyoto/Alloc/CMediumAllocPool.cpp",
+                extra_cflags=(
+                    # TODO: PAL inlining
+                    ['-pragma "inline_max_size(125)"']
+                    if version_num >= VERSIONS.index("GM8P01_00")
+                    else []
+                ),
+            ),
             Object(
                 MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48", "GM8P01_00"),
                 "Kyoto/Alloc/CSmallAllocPool.cpp",
