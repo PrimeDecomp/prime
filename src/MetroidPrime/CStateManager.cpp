122af78ede8e53258c75a5af4331c17e96fc6083
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-13 00:08:12 +0300

    Simplify CStateManager powerup initialization

--- src/MetroidPrime/CStateManager.cpp
+++ src/MetroidPrime/CStateManager.cpp
@@ -865,19 +865,16 @@ void CStateManager::InitializeState(unsigned int mlvlId, TAreaId aid, unsigned i
         gpGameState->SetDeferPowerupInit(false);
 
         for (int i = CPlayerState::kIT_PowerBeam; i < CPlayerState::kIT_Max; ++i) {
-          CPlayerState& state = *x8b8_playerState;
           const CPlayerState::EItemType itemType = static_cast< CPlayerState::EItemType >(i);
-          if (static_cast< int >(state.GetPowerUp(itemType)) < spawnPoint->GetPowerup(itemType)) {
-            CPlayerState& state = *x8b8_playerState;
-            state.InitializePowerUp(itemType,
-                                    spawnPoint->GetPowerup(itemType) - state.GetPowerUp(itemType));
+          if (static_cast< int >(GetPlayerState()->GetPowerUp(itemType)) <
+              spawnPoint->GetPowerup(itemType)) {
+            GetPlayerState()->InitializePowerUp(
+                itemType, spawnPoint->GetPowerup(itemType) - GetPlayerState()->GetPowerUp(itemType));
           }
 
-          CPlayerState& amountState = *x8b8_playerState;
-          if (amountState.GetItemAmount(itemType) < spawnPoint->GetPowerup(itemType)) {
-            CPlayerState& state = *x8b8_playerState;
-            state.IncrPickUp(itemType,
-                             spawnPoint->GetPowerup(itemType) - state.GetItemAmount(itemType));
+          if (GetPlayerState()->GetItemAmount(itemType) < spawnPoint->GetPowerup(itemType)) {
+            GetPlayerState()->IncrPickUp(
+                itemType, spawnPoint->GetPowerup(itemType) - GetPlayerState()->GetItemAmount(itemType));
           }
         }
       }
