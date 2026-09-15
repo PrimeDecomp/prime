5ff6fb76205e7f76b33424382f616c6c4539e717
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 13:23:52 +0300

    Match & link CScriptCounter

--- configure.py
+++ configure.py
@@ -643,7 +643,7 @@ config.libs = [
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/Cameras/CCinematicCamera.cpp"),
             Object(NonMatching, "MetroidPrime/CAutoMapper.cpp"),
             Object(
-                MatchingFor("GM8E01_00", "GM8E01_01"),
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
                 "MetroidPrime/ScriptObjects/CScriptCounter.cpp",
             ),
             Object(NonMatching, "MetroidPrime/CMapWorld.cpp"),
