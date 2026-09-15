58628ba774eced8affcdcb19ca0dca3a0ed4152f
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 13:41:24 +0300

    Match & link CScriptCameraFilterKeyframe

--- configure.py
+++ configure.py
@@ -756,7 +756,7 @@ config.libs = [
             ),
             Object(NonMatching, "MetroidPrime/Player/CPlayerCameraBob.cpp"),
             Object(
-                MatchingFor("GM8E01_00", "GM8E01_01"),
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
                 "MetroidPrime/ScriptObjects/CScriptCameraFilterKeyframe.cpp",
             ),
             Object(
