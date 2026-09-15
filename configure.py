c2ba404908caf7601e3cc83e090cd227a0e5d0e2
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 13:55:32 +0300

    Match & link CSustainedPlayerDamage

--- configure.py
+++ configure.py
@@ -1351,7 +1351,10 @@ config.libs = [
                 NonMatching, "MetroidPrime/ScriptObjects/CScriptProjectedShadow.cpp"
             ),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/ScriptObjects/CEnergyBall.cpp"),
-            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "MetroidPrime/ScriptObjects/CSustainedPlayerDamage.cpp"),
+            Object(
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
+                "MetroidPrime/ScriptObjects/CSustainedPlayerDamage.cpp",
+            ),
             Object(
                 MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
                 "MetroidPrime/Enemies/CPoisonProjectile.cpp",
