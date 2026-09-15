6650c929f994f679d4540c660b5f70a4d659ac26
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 13:24:14 +0300

    Match & link CScriptRelay

--- configure.py
+++ configure.py
@@ -732,7 +732,7 @@ config.libs = [
                 extra_cflags=['-pragma "inline_max_size(250)"'] if config.version == "GM8E01_02" else [],
             ),
             Object(
-                MatchingFor("GM8E01_00", "GM8E01_01"),
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
                 "MetroidPrime/ScriptObjects/CScriptRelay.cpp",
             ),
             Object(
