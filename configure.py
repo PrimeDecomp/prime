62080a54496f20e41c5de8dce41f4e5a2a913ec6
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 13:41:48 +0300

    Match & link CScriptCameraBlurKeyframe

--- configure.py
+++ configure.py
@@ -760,7 +760,7 @@ config.libs = [
                 "MetroidPrime/ScriptObjects/CScriptCameraFilterKeyframe.cpp",
             ),
             Object(
-                MatchingFor("GM8E01_00", "GM8E01_01"),
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
                 "MetroidPrime/ScriptObjects/CScriptCameraBlurKeyframe.cpp",
             ),
             Object(NonMatching, "MetroidPrime/Cameras/CCameraFilter.cpp"),
