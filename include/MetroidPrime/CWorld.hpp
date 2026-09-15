db2a34acf799ba49b42e72c67e38eeec1b3ad587
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 14:56:41 +0300

    Match PAL CWorld sound query

--- include/MetroidPrime/CWorld.hpp
+++ include/MetroidPrime/CWorld.hpp
@@ -147,7 +147,11 @@ public:
   static CGameArea::CConstChainIterator skGlobalEnd;
   void StopGlobalSound(ushort soundId);
   void AddGlobalSound(ushort soundId, CSfxHandle handle);
+#if VERSION >= VERSION_GM8P_00 && VERSION != VERSION_GM8E_02
+  bool IsPlayingSound(uint soundId) const;
+#else
   bool HasGlobalSound(ushort soundId) const;
+#endif
 
   int GetNumAreas() const { return x18_areas.size(); }
   bool AreSkyNeedsMet() const;
