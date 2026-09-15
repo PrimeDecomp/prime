101b720f6e0d1c15824624429434802c0dc2de35
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 11:57:51 +0300

    Link CGuiHeadWidget on PAL

--- configure.py
+++ configure.py
@@ -1434,7 +1434,10 @@ config.libs = [
                 MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
                 "GuiSys/CGuiGroup.cpp",
             ),
-            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "GuiSys/CGuiHeadWidget.cpp"),
+            Object(
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
+                "GuiSys/CGuiHeadWidget.cpp",
+            ),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "GuiSys/CGuiLight.cpp"),
             Object(NonMatching, "GuiSys/CGuiModel.cpp"),
             Object(NonMatching, "GuiSys/CGuiObject.cpp", cflags=[*cflags_retro, "-inline auto"]),
