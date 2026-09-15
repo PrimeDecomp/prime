9a6e4dd44bcfee5d9fd51a36589af3bacf8d30c0
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 12:05:44 +0300

    Link CGuiWidget on PAL

--- configure.py
+++ configure.py
@@ -1461,7 +1461,10 @@ config.libs = [
                 MatchingFor("GM8E01_00", "GM8E01_01"), "GuiSys/CGuiTextPane.cpp"
             ),
             Object(NonMatching, "GuiSys/CGuiTextSupport.cpp"),
-            Object(MatchingFor("GM8E01_00", "GM8E01_01"), "GuiSys/CGuiWidget.cpp"),
+            Object(
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
+                "GuiSys/CGuiWidget.cpp",
+            ),
             Object(MatchingFor("GM8E01_00", "GM8E01_01"), "GuiSys/CGuiWidgetIdDB.cpp"),
             Object(
                 MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"), "GuiSys/CGuiWidgetDrawParms.cpp"
