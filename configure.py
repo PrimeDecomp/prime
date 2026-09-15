75d415ece41aa9135992dc182c15c2332a0cedea
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-15 14:05:27 +0300

    Match & link CScriptCameraWaypoint

--- configure.py
+++ configure.py
@@ -665,7 +665,7 @@ config.libs = [
             Object(MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00", "GM8E01_02"), "MetroidPrime/UserNames.cpp"),
             Object(NonMatching, "MetroidPrime/ScriptObjects/CScriptGenerator.cpp"),
             Object(
-                MatchingFor("GM8E01_00", "GM8E01_01"),
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8P01_00"),
                 "MetroidPrime/ScriptObjects/CScriptCameraWaypoint.cpp",
             ),
             Object(
