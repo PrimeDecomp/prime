9ca9d0836c596875a2b9cc34e84bb5712f916e26
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-09-26 20:01:14 +0300

    Match & link CInputStream for Trilogy (#552)
    * Match & link CInputStream for Trilogy
    
    * Cleanup configure.py and ReadDouble

--- configure.py
+++ configure.py
@@ -58,7 +58,7 @@ DISABLED_VERSIONS = [
     # 4,
     # 5,
     6,
-    7,
+    # 7,
     8,
 ]
 
@@ -316,13 +316,19 @@ cflags_retro = [
     "-use_lmw_stmw on",
     "-str reuse,pool,readonly",
     "-gccinc",
-    "-inline deferred",
+    "-inline deferred" if version_num < VERSIONS.index("R3IJ01_00") else "-inline noauto,nobottomup,level=8",
     "-common on",
     "-i extern/musyx/include",
     "-i extern/rstl/include",
     # "-sym on",
     "-DMUSY_TARGET=MUSY_TARGET_DOLPHIN",
 ]
+
+if version_num >= VERSIONS.index("R3IJ01_00"):
+    cflags_retro.extend([
+        "-sdata 4",
+        "-func_align 4"
+    ])
 
 # Most Retro code uses this inline limit. Objects that still need the compiler
 # default retain cflags_retro explicitly while their helper inlining is investigated.
@@ -422,7 +428,7 @@ def TrkLib(lib_name, objects):
 def RetroLib(lib_name, progress_category, objects):
     return {
         "lib": lib_name + "CW" + "D" if args.debug else "",
-        "mw_version": "GC/1.3.2",
+        "mw_version": "GC/1.3.2" if version_num < VERSIONS.index("R3IJ01_00") else "Wii/1.3",
         "cflags": cflags_retro_inline,
         "progress_category": progress_category,
         "objects": objects,
@@ -433,7 +439,7 @@ def RetroLib(lib_name, progress_category, objects):
 def KyotoLib(lib_name, progress_category, objects):
     return {
         "lib": lib_name + "CW" + "D" if args.debug else "",
-        "mw_version": "GC/1.3.2",
+        "mw_version": "GC/1.3.2" if version_num < VERSIONS.index("R3IJ01_00") else "Wii/1.3",
         "cflags": cflags_retro_inline,
         "host": False,
         "progress_category": progress_category,
@@ -2054,7 +2060,7 @@ config.libs = [
                 src_dir="extern/rstl/src",
             ),
             Object(
-                MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48", "GM8P01_00"),
+                MatchingFor("GM8E01_00", "GM8E01_01", "GM8E01_48", "GM8P01_00", "R3ME01_00"),
                 "Kyoto/Streams/CInputStream.cpp",
             ),
             Object(
